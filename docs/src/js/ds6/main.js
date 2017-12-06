842c4a2cd0a9cf419d4209582876f0cfd4788c34
Ian McBurnie <ianmcburnie@hotmail.com>
2017-12-05 20:39:40 -0800

    Button: new DS6 styles #9

--- docs/src/js/ds6/main.js
+++ docs/src/js/ds6/main.js
@@ -1,1 +1,18 @@
 console.log('skin-ds6');
+
+function nodeListToArray(nodeList) {
+    return Array.prototype.slice.call(nodeList);
+}
+
+nodeListToArray(document.querySelectorAll('.btn:not([aria-disabled="true"])')).forEach(function(el, i) {
+    el.addEventListener('click', function(e) {
+        alert('You clicked ' + this);
+    });
+});
+
+nodeListToArray(document.querySelectorAll('.expand-btn')).forEach(function(el, i) {
+    el.addEventListener('click', function(e) {
+        var isExpanded = this.getAttribute('aria-expanded') === 'true';
+        this.setAttribute('aria-expanded', !isExpanded);
+    })
+});
