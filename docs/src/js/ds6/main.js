f599addad7a66c0d50873413af05e4d248786432
Ian McBurnie <ianmcburnie@hotmail.com>
2017-12-06 16:46:57 -0800

    WIP commit for #10

--- docs/src/js/ds6/main.js
+++ docs/src/js/ds6/main.js
@@ -1,4 +1,6 @@
-console.log('skin-ds6');
+var Expander = require('makeup-expander');
+var Rover = require('makeup-roving-tabindex');
+var scrollKeyPreventer = require('makeup-prevent-scroll-keys');
 
 function nodeListToArray(nodeList) {
     return Array.prototype.slice.call(nodeList);
@@ -10,9 +12,26 @@ nodeListToArray(document.querySelectorAll('.btn:not([aria-disabled="true"])')).f
     });
 });
 
-nodeListToArray(document.querySelectorAll('.expand-btn')).forEach(function(el, i) {
+nodeListToArray(document.querySelectorAll('.expand-btn-example')).forEach(function(el, i) {
     el.addEventListener('click', function(e) {
         var isExpanded = this.getAttribute('aria-expanded') === 'true';
         this.setAttribute('aria-expanded', !isExpanded);
     })
+});
+
+nodeListToArray(document.querySelectorAll('.menu__items')).forEach(function(el, i) {
+    var widget = Rover.createLinear(el, '[role^=menuitem]', {autoReset: 0});
+});
+
+nodeListToArray(document.querySelectorAll('.menu, .fake-menu')).forEach(function(el, i) {
+    var widget = new Expander(el, {
+        click: true,
+        focusManagement: 'interactive',
+        hostSelector: '.expand-btn',
+        contentSelector: '.menu__items, .fake-menu__items'
+    });
+});
+
+nodeListToArray(document.querySelectorAll('[role^=menuitem]')).forEach(function(el, i) {
+    scrollKeyPreventer.add(el);
 });
