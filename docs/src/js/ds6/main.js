fbfa6882ec0b1b970229373f6125a9196d158017
Ian McBurnie <ianmcburnie@hotmail.com>
2017-12-12 13:54:13 -0800

    Menu: new DS6 style #10 PR (#45)

--- docs/src/js/ds6/main.js
+++ docs/src/js/ds6/main.js
@@ -1,18 +1,54 @@
-console.log('skin-ds6');
+// Most of this is code for the menu logic, which will be abstracted away in it's own module soon
 
+// makeup-js accessibility modules
+var Expander = require('makeup-expander');
+var Rover = require('makeup-roving-tabindex');
+var keyEmitter = require('makeup-key-emitter');
+var scrollKeyPreventer = require('makeup-prevent-scroll-keys');
+
+// util function
 function nodeListToArray(nodeList) {
     return Array.prototype.slice.call(nodeList);
 }
 
+// simple button logic on buttons
 nodeListToArray(document.querySelectorAll('.btn:not([aria-disabled="true"])')).forEach(function(el, i) {
     el.addEventListener('click', function(e) {
         alert('You clicked ' + this);
     });
 });
 
-nodeListToArray(document.querySelectorAll('.expand-btn')).forEach(function(el, i) {
+// simple aria-expanded logic on expand button example
+nodeListToArray(document.querySelectorAll('.expand-btn-example')).forEach(function(el, i) {
     el.addEventListener('click', function(e) {
         var isExpanded = this.getAttribute('aria-expanded') === 'true';
         this.setAttribute('aria-expanded', !isExpanded);
     })
+});
+
+// roving tab index logic on menu items container
+nodeListToArray(document.querySelectorAll('[role=menu]')).forEach(function(el, i) {
+    var widget = Rover.createLinear(el, '[role^=menuitem]', {autoReset: 0});
+});
+
+// aria expanded logic on menu button and overlay
+// escape key logic on menu (closes menu)
+nodeListToArray(document.querySelectorAll('.menu, .fake-menu')).forEach(function(el, i) {
+    var widget = new Expander(el, {
+        click: true,
+        focusManagement: 'interactive',
+        hostSelector: '.expand-btn',
+        contentSelector: '.menu__items, .fake-menu__items'
+    });
+
+    keyEmitter.addKeyDown(el);
+
+    el.addEventListener('escapeKeyDown', function() {
+        this.querySelector('.expand-btn').focus();
+    });
+});
+
+// prevent scroll keys logic on menu items
+nodeListToArray(document.querySelectorAll('[role^=menuitem]')).forEach(function(el, i) {
+    scrollKeyPreventer.add(el);
 });
