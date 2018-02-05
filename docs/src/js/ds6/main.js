04502498d64b3a9ee64a9674e8480b3aab9ab09f
Sean Gates <oregonwebdev@gmail.com>
2018-02-05 12:15:45 -0600

    Accessible listbox (#52)
    Closes #38

--- docs/src/js/ds6/main.js
+++ docs/src/js/ds6/main.js
@@ -54,30 +54,38 @@ nodeListToArray(document.querySelectorAll('[role^=menuitem]')).forEach(function(
     scrollKeyPreventer.add(el);
 });
 
-// roving tab index logic on menu items container
+// roving tab index logic on listbox options container
 nodeListToArray(document.querySelectorAll('[role=listbox]')).forEach(function(el, i) {
     var widget = Rover.createLinear(el, '[role^=option]', {autoReset: 0});
 });
 
-// aria expanded logic on menu button and overlay
-// escape key logic on menu (closes menu)
+// aria expanded logic on listbox input and overlay
+// escape key logic on listbox (closes listbox)
 nodeListToArray(document.querySelectorAll('.listbox')).forEach(function(el, i) {
     var widget = new Expander(el, {
         autoCollapse: true,
         click: true,
         focusManagement: 'interactive',
-        hostSelector: '.expand-btn',
-        contentSelector: '.listbox__options'
+        hostSelector: '.listbox__control > input',
+        hostContainerClass: 'listbox__control',
+        contentSelector: '.listbox__options',
+        spacebar: true
     });
 
     keyEmitter.addKeyDown(el);
 
     el.addEventListener('escapeKeyDown', function() {
-        this.querySelector('.expand-btn').focus();
+        widget.collapse();
+        this.querySelector('.listbox__control > input').focus();
     });
 });
 
-// prevent scroll keys logic on menu items
+// prevent scroll keys logic on the listbox button
+nodeListToArray(document.querySelectorAll('.listbox__control > input')).forEach(function (el, i) {
+    scrollKeyPreventer.add(el);
+});
+
+// prevent scroll keys logic on listbox options
 nodeListToArray(document.querySelectorAll('[role^=option]')).forEach(function(el, i) {
     scrollKeyPreventer.add(el);
 });
