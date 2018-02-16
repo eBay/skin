c9648331e1f72db809ec89c45b91f72adc0f71df
Ian McBurnie <ianmcburnie@hotmail.com>
2018-02-15 16:13:00 -0800

    Added basic kb & mouse interaction to listbox examples #38

--- docs/src/js/ds6/main.js
+++ docs/src/js/ds6/main.js
@@ -1,4 +1,4 @@
-// Most of this is code for the menu logic, which will be abstracted away in it's own module soon
+// Most of this is code for the menu and listbox logic, which will be abstracted away into their own modules at some point
 
 // makeup-js accessibility modules
 var Expander = require('makeup-expander');
@@ -10,6 +10,8 @@ var scrollKeyPreventer = require('makeup-prevent-scroll-keys');
 function nodeListToArray(nodeList) {
     return Array.prototype.slice.call(nodeList);
 }
+
+// BUTTON WIDGET
 
 // simple button logic on buttons
 nodeListToArray(document.querySelectorAll('.btn:not([aria-disabled="true"])')).forEach(function(el, i) {
@@ -25,6 +27,8 @@ nodeListToArray(document.querySelectorAll('.expand-btn-example')).forEach(functi
         this.setAttribute('aria-expanded', !isExpanded);
     })
 });
+
+// MENU WIDGET (basic interactivity only)
 
 // roving tab index logic on menu items container
 nodeListToArray(document.querySelectorAll('[role=menu]')).forEach(function(el, i) {
@@ -54,38 +58,61 @@ nodeListToArray(document.querySelectorAll('[role^=menuitem]')).forEach(function(
     scrollKeyPreventer.add(el);
 });
 
-// roving tab index logic on listbox options container
-nodeListToArray(document.querySelectorAll('[role=listbox]')).forEach(function(el, i) {
-    var widget = Rover.createLinear(el, '[role^=option]', {autoReset: 0});
-});
+// LISTBOX WIDGET (basic interactivity only)
 
-// aria expanded logic on listbox input and overlay
-// escape key logic on listbox (closes listbox)
 nodeListToArray(document.querySelectorAll('.listbox')).forEach(function(el, i) {
     var widget = new Expander(el, {
         autoCollapse: true,
         click: true,
-        focusManagement: 'interactive',
-        hostSelector: '.listbox__control > input',
+        hostSelector: 'input[role=combobox]',
         hostContainerClass: 'listbox__control',
         contentSelector: '.listbox__options',
         spacebar: true
     });
 
+    var inputEl = el.querySelector('input[role=combobox]');
+    var optionEls = nodeListToArray(el.querySelectorAll('[role=option]'));
+    var selectedOptionEl = el.querySelector('[role=option][aria-selected=true]');
+    var size = optionEls.length;
+    var currentIndex;
+
+    var updateListbox = function(newIndex) {
+        selectedOptionEl.setAttribute('aria-selected', 'false');
+        currentIndex = newIndex;
+        inputEl.value = 'Option ' + (currentIndex + 1);
+        selectedOptionEl = optionEls[currentIndex];
+        selectedOptionEl.setAttribute('aria-selected', 'true');
+    };
+
+    optionEls.forEach(function(el, i) {
+        // assign an index data attribute to each el
+        el.dataset.listboxIndex = i;
+
+        // add a click handler to each el
+        el.addEventListener('click', function(e) {
+            updateListbox(parseInt(this.dataset.listboxIndex, 10));
+        });
+    });
+
+    currentIndex = parseInt(selectedOptionEl.dataset.listboxIndex, 10);
+
     keyEmitter.addKeyDown(el);
+    scrollKeyPreventer.add(el);
 
     el.addEventListener('escapeKeyDown', function() {
         widget.collapse();
-        this.querySelector('.listbox__control > input').focus();
+        inputEl.focus();
     });
-});
 
-// prevent scroll keys logic on the listbox button
-nodeListToArray(document.querySelectorAll('.listbox__control > input')).forEach(function (el, i) {
-    scrollKeyPreventer.add(el);
-});
+    el.addEventListener('arrowDownKeyDown', function(e) {
+        if (currentIndex < (size - 1)) {
+            updateListbox(currentIndex + 1);
+        }
+    });
 
-// prevent scroll keys logic on listbox options
-nodeListToArray(document.querySelectorAll('[role^=option]')).forEach(function(el, i) {
-    scrollKeyPreventer.add(el);
+    el.addEventListener('arrowUpKeyDown', function(e) {
+        if (currentIndex > 0) {
+            updateListbox(currentIndex - 1);
+        }
+    });
 });
