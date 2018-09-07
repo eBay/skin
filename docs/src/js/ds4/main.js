df7b16292dec8b6011bc24008eca5bdce0595283
prpandurangan <prpandurangan@ebay.com>
2018-09-07 16:39:03 -0700

    DS6 tooltip component

--- docs/src/js/ds4/main.js
+++ docs/src/js/ds4/main.js
@@ -3,7 +3,7 @@ $('#header-large .grid__cell').skipTo();
 $('.fake-menu').clickFlyout({closeOnEsc: true, focusManagement: 'first', triggerSelector: '.expand-btn', overlaySelector: '.fake-menu__items'});
 $('.menu').menu();
 $('.tabs').tabs();
-$('.infotip').tooltip(); /****** to be discussed ******/
+$('.tooltip').tooltip();
 $('.bubblehelp').clickFlyout({triggerSelector: '.bubblehelp__host', overlaySelector: '.bubblehelp__overlay', autoCollapse: false});
 $('#banner-menu').on('click', 'a', function(e) {
     var target = document.querySelector(this.href.substring(this.href.indexOf('#')));
