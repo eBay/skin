fc19b7222f1e8ea7d41124b19834525e0c25423d
Ian McBurnie <ianmcburnie@hotmail.com>
2018-10-17 21:50:47 -0700

    Website: stripping out more ds4 docs js (#419)

--- docs/src/js/ds4/main.js
+++ docs/src/js/ds4/main.js
@@ -7,20 +7,19 @@ function querySelectorAllToArray(selector, parentNode) {
     return nodeListToArray(parentNode.querySelectorAll(selector));
 }
 
-$('#header-large .grid__cell').skipTo();
-// $('.dialog-button').dialogButton();
 $('.tooltip').tooltip();
 $('.bubblehelp').clickFlyout({triggerSelector: '.bubblehelp__host', overlaySelector: '.bubblehelp__overlay', autoCollapse: false});
-$('#banner-menu').on('click', 'a', function(e) {
-    var target = document.querySelector(this.href.substring(this.href.indexOf('#')));
-    target.setAttribute('tabindex', '-1');
-    target.focus();
-});
-$('.expand-btn-example').on('click', function() {
-    $(this).attr('aria-expanded', ($(this).attr('aria-expanded') === 'true') ? 'false' : 'true');
+
+// EXPAND BUTTONS - Toggle state
+querySelectorAllToArray('.expand-btn-example').forEach(function(el, index) {
+    var $buttonEl = $(el);
+
+    $buttonEl.on('click', function(e) {
+        $buttonEl.attr('aria-expanded', ($buttonEl.attr('aria-expanded') === 'true') ? 'false' : 'true');
+    });
 });
 
-// COMBOBOX WIDGET - Expand/Collapse only
+// COMBOBOX WIDGETS - Expand/Collapse only
 querySelectorAllToArray('.combobox').forEach(function(el, index) {
     var $inputWrapperEl = $(el.querySelector('.combobox__control'));
     var $inputEl = $(el.querySelector('input'));
@@ -55,25 +54,19 @@ querySelectorAllToArray('.listbox, .menu, .fake-menu').forEach(function(el) {
     });
 });
 
+// DIALOG BUTTONS - Open/Close dialog only (no modal behaviour)
 querySelectorAllToArray('.dialog-button').forEach(function(btn) {
-    // var cancel;
     var dialog = btn.nextElementSibling;
     var dialogBody = dialog.querySelector('.dialog__body');
     var dialogClose = dialog.querySelector('.dialog__close');
     btn.addEventListener('click', handleOpen);
 
     function handleOpen() {
-        // if (cancel) {
-        //     cancel();
-        // }
-
-        // cancel = transition(dialog, 'dialog--show', handleTransitionEnd);
         dialog.removeAttribute('hidden');
         btn.removeEventListener('click', handleOpen);
         dialog.addEventListener('click', handleClose, true);
         document.body.setAttribute("style", "overflow:hidden");
         dialogClose.focus();
-        // modal.modal(dialog);
     }
 
     function handleClose(ev) {
@@ -81,20 +74,11 @@ querySelectorAllToArray('.dialog-button').forEach(function(btn) {
             return;
         }
 
-        // if (cancel) {
-        //     cancel();
-        // }
-
-        // cancel = transition(dialog, 'dialog--hide', handleTransitionEnd);
         dialog.setAttribute('hidden', '');
         btn.addEventListener('click', handleOpen);
         dialog.removeEventListener('click', handleClose, true);
         document.body.removeAttribute("style");
-        // modal.unmodal();
+
         btn.focus();
     }
-
-    // function handleTransitionEnd() {
-    //     cancel = undefined;
-    // }
 });
