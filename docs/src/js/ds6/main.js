2d206137a795e75803f5ee650cec6b86aff76db5
Ian McBurnie <imcburnie@ebay.com>
2018-10-19 10:53:07 -0700

    Website: removed jQuery and all references

--- docs/src/js/ds6/main.js
+++ docs/src/js/ds6/main.js
@@ -1,45 +1,5 @@
-// Most of this is code for the menu and combobox logic, which will be abstracted away into their own modules at some point
-
-// makeup-js accessibility modules
 var Expander = require('makeup-expander');
-var scrollKeyPreventer = require('makeup-prevent-scroll-keys');
-var modal = require('makeup-modal');
-var transition = require("./transition");
 var FloatingLabel = require('makeup-floating-label');
-
-// util functions
-function nodeListToArray(nodeList) {
-    return Array.prototype.slice.call(nodeList);
-}
-
-function querySelectorAllToArray(selector, parentNode) {
-    parentNode = parentNode || document;
-    return nodeListToArray(parentNode.querySelectorAll(selector));
-}
-
-// EXPAND BUTTONS - Toggle state
-querySelectorAllToArray('.expand-btn-example').forEach(function(el, i) {
-    el.addEventListener('click', function(e) {
-        var isExpanded = this.getAttribute('aria-expanded') === 'true';
-        this.setAttribute('aria-expanded', !isExpanded);
-    })
-});
-
-// LISTBOX, MENU & FAKE MENU WIDGETS - EXPAND/COLLAPSE ONLY
-querySelectorAllToArray('.listbox, .menu, .fake-menu').forEach(function(widgetEl, widgetIndex) {
-    var buttonEl = widgetEl.querySelector('button');
-
-    if (buttonEl) {
-        buttonEl.addEventListener('click', function(e) {
-            var isExpanded = buttonEl.getAttribute('aria-expanded') === 'true';
-            buttonEl.setAttribute('aria-expanded', !isExpanded);
-        });
-
-        buttonEl.addEventListener('blur', function(e) {
-            buttonEl.setAttribute('aria-expanded', 'false');
-        });
-    }
-});
 
 // TOOLTIP WIDGETS
 querySelectorAllToArray('.tooltip.tooltip--hover').forEach(function(el, i) {
@@ -75,77 +35,8 @@ querySelectorAllToArray('.tooltip:not(.tooltip--hover)').forEach(function(el, i)
     }
 });
 
-// COMBOBOX WIDGETS (basic expand/collapse only)
-querySelectorAllToArray('.combobox').forEach(function(widgetEl, widgetIndex) {
-    var inputWrapperEl = widgetEl.querySelector('.combobox__control');
-    var inputEl = widgetEl.querySelector('input');
-
-    inputEl.addEventListener('focus', function(e) {
-        var isExpanded = inputEl.getAttribute('aria-expanded') === 'true';
-
-        if (isExpanded) {
-            inputWrapperEl.classList.remove('combobox__control--expanded');
-        } else {
-            inputWrapperEl.classList.add('combobox__control--expanded');
-        }
-
-        inputEl.setAttribute('aria-expanded', !isExpanded);
-    });
-
-    inputEl.addEventListener('blur', function(e) {
-        inputEl.setAttribute('aria-expanded', 'false');
-        inputWrapperEl.classList.remove('combobox__control--expanded');
-    });
-});
-
-// DIALOG WIDGETS
-querySelectorAllToArray('.dialog-button').forEach(function (btn) {
-    var cancel;
-    var dialog = btn.nextElementSibling;
-    var dialogBody = dialog.querySelector('.dialog__body');
-    var dialogClose = dialog.querySelector('.dialog__close');
-    btn.addEventListener('click', handleOpen);
-
-    function handleOpen () {
-        if (cancel) {
-            cancel();
-        }
-
-        cancel = transition(dialog, 'dialog--show', handleTransitionEnd(true));
-        dialog.removeAttribute('hidden');
-        btn.removeEventListener('click', handleOpen);
-        dialog.addEventListener('click', handleClose, true);
-        document.body.setAttribute("style", "overflow:hidden");
-        modal.modal(dialog.querySelector('.dialog__window'));
-        dialogClose.focus();
-    }
-
-    function handleClose (ev) {
-        if (dialogBody.contains(ev.target)) {
-            return;
-        }
-
-        if (cancel) {
-            cancel();
-        }
-
-        cancel = transition(dialog, 'dialog--hide', handleTransitionEnd(false));
-        dialog.setAttribute('hidden', '');
-        btn.addEventListener('click', handleOpen);
-        dialog.removeEventListener('click', handleClose, true);
-        document.body.removeAttribute("style");
-        modal.unmodal();
-        btn.focus();
-    }
-
-    function handleTransitionEnd (isOpening) {
-        // focus on the close button
-        if (isOpening) {
-            dialogClose.focus();
-        }
-
-        cancel = undefined;
-    }
+querySelectorAllToArray('.floating-label').forEach(function(el) {
+    var floatingLabel = new FloatingLabel(el);
 });
 
 // set up initial matchMedia query
@@ -192,8 +83,4 @@ window.addEventListener('load', function() {
     fixTheSidebar();
 
     window.addEventListener('scroll', fixTheSidebar);
-});
-
-querySelectorAllToArray('.floating-label').forEach(function(el) {
-    var floatingLabel = new FloatingLabel(el);
 });
