533ee3c2439f774fb0f0aeb1a30ccc1d710035e0
Ian McBurnie <ianmcburnie@hotmail.com>
2018-11-28 10:55:33 -0800

    New Module: Tooltip (#467)

--- docs/src/js/base/main.js
+++ docs/src/js/base/main.js
@@ -100,3 +100,28 @@ querySelectorAllToArray('.dialog-button').forEach(function (btn) {
         cancel = undefined;
     }
 });
+
+var Expander = require('makeup-expander');
+
+// TOOLTIP WIDGETS
+querySelectorAllToArray('.tooltip').forEach(function(el, i) {
+    var widget = new Expander(el, {
+        contentSelector: '.tooltip__overlay',
+        collapseOnFocusOut: true,
+        collapseOnMouseOut: true,
+        expandOnFocus: true,
+        expandOnHover: true,
+        focusManagement: 'focusable',
+        hostSelector: '.tooltip__host'
+    });
+});
+
+// INFOTIP/BUBBLEHELP WIDGETS
+querySelectorAllToArray('.infotip').forEach(function(el, i) {
+    var widget = new Expander(el, {
+        contentSelector: '.infotip__overlay',
+        expandOnFocus: false,
+        expandOnClick: true,
+        hostSelector: '.infotip__host'
+    });
+});
