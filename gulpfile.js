cb62da39b1c27f5507d3ab8b5ea01dd28a053dec
Ian McBurnie <imcburnie@ebay.com>
2019-07-28 10:39:14 -0700

    Button: separate CTA button #679

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,9 @@ function modules () {
        './src/less/**/listbox-button.less',
        '!./src/less/listbox-button/base/listbox-button.less',
        './src/less/**/menu-button.less',
-       '!./src/less/menu-button/base/menu-button.less'
+       '!./src/less/menu-button/base/menu-button.less',
+       './src/less/**/cta-button.less',
+       '!./src/less/cta-button/base/cta-button.less'
     ])
     .pipe(less({plugins: [autoprefixPlugin]}))
     .pipe(gulp.dest(distTarget))
