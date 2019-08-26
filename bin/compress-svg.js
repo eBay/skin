4ff3342c7feebd4d15f6c8e8d087b3787a6fe0cd
Andrew Gliga <agliga@ebay.com>
2019-08-26 14:17:34 -0700

    Cleanup and review comments

--- bin/compress-svg.js
+++ bin/compress-svg.js
@@ -1,112 +1,89 @@
 #!/usr/bin/env node
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
+const Svgo = require('svgo');
+const files = [
+  path.resolve(__dirname, '../', 'src', 'svg', 'ds6', 'icons.svg'),
+  path.resolve(__dirname, '../', 'src', 'svg', 'ds4', 'icons.svg'),
+];
+const svgo = new Svgo({
+  plugins: [{
+    "cleanupAttrs": true
+  }, {
+    "removeDoctype": true
+  }, {
+    "removeXMLProcInst": false
+  }, {
+    "removeComments": false
+  }, {
+    "removeMetadata": true
+  }, {
+    "removeTitle": true
+  }, {
+    "removeDesc": true
+  }, {
+    "removeUselessDefs": false
+  }, {
+    "removeEditorsNSData": true
+  }, {
+    "removeEmptyAttrs": true
+  }, {
+    "removeHiddenElems": false
+  }, {
+    "removeEmptyText": true
+  }, {
+    "removeEmptyContainers": true
+  }, {
+    "removeViewBox": false
+  }, {
+    "cleanupEnableBackground": true
+  }, {
+    "convertStyleToAttrs": true
+  }, {
+    "convertColors": true
+  }, {
+    "convertPathData": true
+  }, {
+    "convertTransform": true
+  }, {
+    "removeUnknownsAndDefaults": true
+  }, {
+    "removeNonInheritableGroupAttrs": true
+  }, {
+    "removeUselessStrokeAndFill": false
+  }, {
+    "removeUnusedNS": true
+  }, {
+    "minifyStyles": false
+  }, {
+    "cleanupIDs": false
+  }, {
+    "cleanupNumericValues": true
+  }, {
+    "moveElemsAttrsToGroup": true
+  }, {
+    "moveGroupAttrsToElems": true
+  }, {
+    "collapseGroups": true
+  }, {
+    "removeRasterImages": false
+  }, {
+    "mergePaths": true
+  }, {
+    "convertShapeToPath": true
+  }, {
+    "sortAttrs": true
+  }, {
+    "removeDimensions": true
+  }],
+  js2svg: { pretty: true }
 
-var FS = require('fs'),
-  PATH = require('path'),
-  SVGO = require('svgo'),
-  files = [
-    PATH.resolve(__dirname, '../', 'src', 'svg', 'ds6', 'icons.svg'),
-    PATH.resolve(__dirname, '../', 'src', 'svg', 'ds4', 'icons.svg'),
-  ],
-  svgo = new SVGO({
-    plugins: [{
-      "cleanupAttrs": true
-    }, {
-      "removeDoctype": true
-    }, {
-      "removeXMLProcInst": false
-    }, {
-      "removeComments": false
-    }, {
-      "removeMetadata": true
-    }, {
-      "removeTitle": true
-    }, {
-      "removeDesc": true
-    }, {
-      "removeUselessDefs": false
-    }, {
-      "removeEditorsNSData": true
-    }, {
-      "removeEmptyAttrs": true
-    }, {
-      "removeHiddenElems": false
-    }, {
-      "removeEmptyText": true
-    }, {
-      "removeEmptyContainers": true
-    }, {
-      "removeViewBox": false
-    }, {
-      "cleanupEnableBackground": true
-    }, {
-      "convertStyleToAttrs": true
-    }, {
-      "convertColors": true
-    }, {
-      "convertPathData": true
-    }, {
-      "convertTransform": true
-    }, {
-      "removeUnknownsAndDefaults": true
-    }, {
-      "removeNonInheritableGroupAttrs": true
-    }, {
-      "removeUselessStrokeAndFill": false
-    }, {
-      "removeUnusedNS": true
-    }, {
-      "minifyStyles": false
-    }, {
-      "cleanupIDs": false
-    }, {
-      "cleanupNumericValues": true
-    }, {
-      "moveElemsAttrsToGroup": true
-    }, {
-      "moveGroupAttrsToElems": true
-    }, {
-      "collapseGroups": true
-    }, {
-      "removeRasterImages": false
-    }, {
-      "mergePaths": true
-    }, {
-      "convertShapeToPath": true
-    }, {
-      "sortAttrs": true
-    }, {
-      "removeDimensions": true
-    }],
-    js2svg: { pretty: true }
+});
 
-  });
-
-files.forEach((filePath) => {
-
-  FS.readFile(filePath, 'utf8', function (err, data) {
-
-    if (err) {
-      throw err;
-    }
-
-
-    svgo.optimize(data, { path: filePath }).then(function (result) {
-      FS.writeFileSync(result.path, result.data);
-      // {
-      //     // optimized SVG data string
-      //     data: '<svg width="10" height="20">test</svg>'
-      //     // additional info such as width/height
-      //     info: {
-      //         width: '10',
-      //         height: '20'
-      //     }
-      // }
-
-    });
-
-  });
-
-
+files.forEach(async (filePath) => {
+  const data = await fs.promises.readFile(filePath, 'utf8');
+  const result = await svgo.optimize(data, { path: filePath });
+  fs.promises.writeFile(result.path, result.data);
 });
