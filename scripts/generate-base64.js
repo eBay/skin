3bdced592446c9331af62fc2d4a8006942cb975f
Andrew Gliga <agliga@ebay.com>
2019-12-18 10:37:49 -0800

    Split up image script to have base64 and normal image optimization

--- scripts/generate-base64.js
+++ scripts/generate-base64.js
@@ -5,5 +5,6 @@ const helper = require('./generate-helper');
 
 helper.runner(async (data) => {
   const result = await helper.optimizeSVG(data.filePath);
-  await helper.writeSymbols(result, data.dsVersion);
+  const svgGenerator = new helper.SVGGenerator(result, data.dsVersion);
+  await svgGenerator.generateAllBase64();
 });
