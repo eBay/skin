1c1fbd41a1d35a6c4b11a4277fbc850fda926800
Raveena Kothapally <rkothapally@ebay.com>
2020-01-29 02:48:33 -0800

    More storybook cases added

---
+++ src/less/filter-menu/filter-menu.stories.js
@@ -0,0 +1,134 @@
+export default { title: 'filter-menu' };
+
+export const defaultFilterMenu = () => `
+<span class="filter-menu">
+    <div class="filter-menu__items" role="menu">
+        <div class="filter-menu__item" role="menuitemcheckbox" tabindex="0" aria-checked="false">
+            <span class="filter-menu__checkbox">
+                <svg class="icon icon--checkbox-unchecked" focusable="false" height="18" width="18">
+                    <use xlink:href="#icon-checkbox-unchecked"></use>
+                </svg>
+                <svg class="icon icon--checkbox-checked" focusable="false" height="18" width="18">
+                    <use xlink:href="#icon-checkbox-checked"></use>
+                </svg>
+            </span>
+            <span class="filter-menu__text">Item 1 with a long string that will wrap to another line</span>
+        </div>
+        <div class="filter-menu__item" role="menuitemcheckbox" aria-checked="false">
+            <span class="filter-menu__checkbox">
+                <svg class="icon icon--checkbox-unchecked" focusable="false" height="18" width="18">
+                    <use xlink:href="#icon-checkbox-unchecked"></use>
+                </svg>
+                <svg class="icon icon--checkbox-checked" focusable="false" height="18" width="18">
+                    <use xlink:href="#icon-checkbox-checked"></use>
+                </svg>
+            </span>
+            <span class="filter-menu__text">Item 2</span>
+        </div>
+        <div class="filter-menu__item" role="menuitemcheckbox" aria-checked="false">
+            <span class="filter-menu__checkbox">
+                <svg class="icon icon--checkbox-unchecked" focusable="false" height="18" width="18">
+                    <use xlink:href="#icon-checkbox-unchecked"></use>
+                </svg>
+                <svg class="icon icon--checkbox-checked" focusable="false" height="18" width="18">
+                    <use xlink:href="#icon-checkbox-checked"></use>
+                </svg>
+            </span>
+            <span class="filter-menu__text">Item 3</span>
+        </div>
+    </div>
+</span>
+`;
+
+export const withFooterButton = () => `
+<span class="filter-menu">
+    <div class="filter-menu__items" role="menu">
+        <div class="filter-menu__item" role="menuitemcheckbox" tabindex="0" aria-checked="false">
+            <span class="filter-menu__checkbox">
+                <svg class="icon icon--checkbox-unchecked" focusable="false" height="18" width="18">
+                    <use xlink:href="#icon-checkbox-unchecked"></use>
+                </svg>
+                <svg class="icon icon--checkbox-checked" focusable="false" height="18" width="18">
+                    <use xlink:href="#icon-checkbox-checked"></use>
+                </svg>
+            </span>
+            <span class="filter-menu__text">Item 1 with a long string that will wrap to another line</span>
+        </div>
+        <div class="filter-menu__item" role="menuitemcheckbox" aria-checked="false">
+            <span class="filter-menu__checkbox">
+                <svg class="icon icon--checkbox-unchecked" focusable="false" height="18" width="18">
+                    <use xlink:href="#icon-checkbox-unchecked"></use>
+                </svg>
+                <svg class="icon icon--checkbox-checked" focusable="false" height="18" width="18">
+                    <use xlink:href="#icon-checkbox-checked"></use>
+                </svg>
+            </span>
+            <span class="filter-menu__text">Item 2</span>
+        </div>
+        <div class="filter-menu__item" role="menuitemcheckbox" aria-checked="false">
+            <span class="filter-menu__checkbox">
+                <svg class="icon icon--checkbox-unchecked" focusable="false" height="18" width="18">
+                    <use xlink:href="#icon-checkbox-unchecked"></use>
+                </svg>
+                <svg class="icon icon--checkbox-checked" focusable="false" height="18" width="18">
+                    <use xlink:href="#icon-checkbox-checked"></use>
+                </svg>
+            </span>
+            <span class="filter-menu__text">Item 3</span>
+        </div>
+    </div>
+    <button type="button" class="filter-menu__footer">Apply</button>
+</span>
+`;
+
+export const formVersion = () => `
+<span class="filter-menu-form">
+    <form name="filter-menu-form-1">
+        <div class="filter-menu-form__items">
+            <label for="filter-menu-form-checkbox-item-1" class="filter-menu-form__item">
+                <span class="checkbox">
+                    <input aria-label="filter menu checkbox example option 1" class="checkbox__control" type="checkbox" name="filter-menu-form-checkbox-item-1" id="filter-menu-form-checkbox-item-1" />
+                    <span class="checkbox__icon" hidden>
+                        <svg class="checkbox__unchecked" focusable="false" height="18" width="18">
+                            <use xlink:href="#icon-checkbox-unchecked"></use>
+                        </svg>
+                        <svg class="checkbox__checked" focusable="false" height="18" width="18">
+                            <use xlink:href="#icon-checkbox-checked"></use>
+                        </svg>
+                    </span>
+                </span>
+                <span class="filter-menu-form__text">Item 1 with a long string that will wrap to another line</span>
+            </label>
+            <label for="filter-menu-form-checkbox-item-2" class="filter-menu-form__item">
+                <span class="checkbox">
+                    <input aria-label="filter menu checkbox example option 2" class="checkbox__control" type="checkbox" name="filter-menu-form-checkbox-item-2" id="filter-menu-form-checkbox-item-2" />
+                    <span class="checkbox__icon" hidden>
+                        <svg class="checkbox__unchecked" focusable="false" height="18" width="18">
+                            <use xlink:href="#icon-checkbox-unchecked"></use>
+                        </svg>
+                        <svg class="checkbox__checked" focusable="false" height="18" width="18">
+                            <use xlink:href="#icon-checkbox-checked"></use>
+                        </svg>
+                    </span>
+                </span>
+                <span for="filter-menu-form-checkbox-item-2" class="filter-menu-form__text">Item 2</span>
+            </label>
+            <label for="filter-menu-form-checkbox-item-3" class="filter-menu-form__item">
+                <span class="checkbox">
+                    <input aria-label="filter menu checkbox example option 3" class="checkbox__control" type="checkbox" name="filter-menu-form-checkbox-item-3" id="filter-menu-form-checkbox-item-3" />
+                    <span class="checkbox__icon" hidden>
+                        <svg class="checkbox__unchecked" focusable="false" height="18" width="18">
+                            <use xlink:href="#icon-checkbox-unchecked"></use>
+                        </svg>
+                        <svg class="checkbox__checked" focusable="false" height="18" width="18">
+                            <use xlink:href="#icon-checkbox-checked"></use>
+                        </svg>
+                    </span>
+                </span>
+                <span for="filter-menu-form-checkbox-item-3" class="filter-menu-form__text">Item 3</span>
+            </label>
+        </div>
+        <button type="submit" class="filter-menu-form__footer">Apply</button>
+    </form>
+</span>
+`;
