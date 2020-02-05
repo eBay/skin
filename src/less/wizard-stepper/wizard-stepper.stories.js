e675d77c9ece333c7b4408b2ca2fc698285dfc2c
Raveena Kothapally <rkothapally@ebay.com>
2020-02-05 13:42:24 -0800

    wizard stepper list implementation

---
+++ src/less/wizard-stepper/wizard-stepper.stories.js
@@ -0,0 +1,14 @@
+export default { title: 'wizardStepperList' };
+
+export const singleLine = () => `
+<div class="wizard-stepper-list">
+    <div class="wizard-step-container">
+        <svg class="icon icon--confirmation-filled" focusable="false" height="24" width="24">
+            <use xlink:href="#icon-confirmation-filled"></use>
+        </svg>
+        <span class="wizard-step-title">
+            Started
+        </span>
+    </div>
+</div>
+`;
