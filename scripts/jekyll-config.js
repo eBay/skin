7f1a426c2f4085196efd55953ecacd2ce1b0ac5f
Ian McBurnie <38065+ianmcburnie@users.noreply.github.com>
2020-02-20 10:23:37 -0800

    Website: update script to add skin version to jekyll defaults (#1035)

--- scripts/jekyll-config.js
+++ scripts/jekyll-config.js
@@ -1,8 +1,8 @@
 /*
- * Updates the CDN paths with the current version of NPM
+ * Updates the Jekyll front matter defaults with the current skin package version from NPM.
  */
 const fs = require('fs');
-const files = ['./docs/index.html', './docs/ds4/index.html'];
+const files = ['./docs/_config.yml'];
 
 files.forEach(file => {
     const newContents = fs.readFileSync(file, 'utf8').replace(/version\:.*\n/gi, `version: ${process.env.npm_package_version}\n`);
