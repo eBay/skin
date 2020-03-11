1972397d4710ce8d3b697e57ac13e8c758cb4e62
Raveena Kothapally <rkothapally@ebay.com>
2020-03-11 01:30:28 -0700

    Add remaining states, tests and storybook

--- src/less/wizard-stepper/wizard-stepper.stories.js
+++ src/less/wizard-stepper/wizard-stepper.stories.js
@@ -1,14 +1,79 @@
-export default { title: 'wizardStepperList' };
+export default { title: 'WizardStepper' };
 
-export const singleLine = () => `
-<div class="wizard-stepper-list">
-    <div class="wizard-step-container">
-        <svg class="icon icon--confirmation-filled" focusable="false" height="24" width="24">
-            <use xlink:href="#icon-confirmation-filled"></use>
-        </svg>
-        <span class="wizard-step-title">
-            Started
-        </span>
-    </div>
+export const wizardStepperDefault = () => `
+<div class="wizard-stepper">
+    <ul class="wizard-stepper__items">
+        <li class="wizard-stepper__item wizard-stepper__item--confirmation">
+            <span class="wizard-stepper__icon">
+                <svg class="icon" focusable="false" height="24" width="24">
+                    <use xlink:href="#icon-confirmation-filled"></use>
+                </svg>
+            </span>
+            <span class="wizard-stepper__text">Started</span>
+        </li>
+        <li class="wizard-stepper__item wizard-stepper__item--confirmation">
+            <span class="wizard-stepper__icon">
+                <svg class="icon" focusable="false" height="24" width="24">
+                    <use xlink:href="#icon-confirmation-filled"></use>
+                </svg>
+            </span>
+            <span class="wizard-stepper__text">Shipped</span>
+        </li>
+        <li class="wizard-stepper__item wizard-stepper__item--inprogress">
+            <span class="wizard-stepper__icon">
+                <svg class="icon" focusable="false" height="24" width="24">
+                    <use xlink:href="#icon-circle"></use>
+                </svg>
+            </span>
+            <span class="wizard-stepper__text">Transit</span>
+        </li>
+        <li class="wizard-stepper__item wizard-stepper__item--upcoming">
+            <span class="wizard-stepper__icon">
+                <svg class="icon" focusable="false" height="24" width="24">
+                    <use xlink:href="#icon-circle"></use>
+                </svg>
+            </span>
+            <span class="wizard-stepper__text">Delivered</span>
+        </li>
+    </ul>
+</div>
+`;
+
+export const wizardStepperVertical = () => `
+<div class="wizard-stepper wizard-stepper--vertical">
+    <ul class="wizard-stepper__items">
+        <li class="wizard-stepper__item wizard-stepper__item--confirmation">
+            <span class="wizard-stepper__icon">
+                <svg class="icon" focusable="false" height="24" width="24">
+                    <use xlink:href="#icon-confirmation-filled"></use>
+                </svg>
+            </span>
+            <span class="wizard-stepper__text">Started</span>
+        </li>
+        <li class="wizard-stepper__item wizard-stepper__item--confirmation">
+            <span class="wizard-stepper__icon">
+                <svg class="icon" focusable="false" height="24" width="24">
+                    <use xlink:href="#icon-confirmation-filled"></use>
+                </svg>
+            </span>
+            <span class="wizard-stepper__text">Shipped</span>
+        </li>
+        <li class="wizard-stepper__item wizard-stepper__item--inprogress">
+            <span class="wizard-stepper__icon">
+                <svg class="icon" focusable="false" height="24" width="24">
+                    <use xlink:href="#icon-circle"></use>
+                </svg>
+            </span>
+            <span class="wizard-stepper__text">Transit</span>
+        </li>
+        <li class="wizard-stepper__item wizard-stepper__item--upcoming">
+            <span class="wizard-stepper__icon">
+                <svg class="icon" focusable="false" height="24" width="24">
+                    <use xlink:href="#icon-circle"></use>
+                </svg>
+            </span>
+            <span class="wizard-stepper__text">Delivered</span>
+        </li>
+    </ul>
 </div>
 `;
