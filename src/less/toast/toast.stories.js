bbe7a97183627f679f85b234ba733c36003afa1d
Raveena Kothapally <rkothapally@ebay.com>
2020-04-09 13:43:27 -0700

    Incorporate review comments for HTML

--- src/less/toast/toast.stories.js
+++ src/less/toast/toast.stories.js
@@ -1,22 +1,22 @@
 export default { title: 'Toast' };
 
 export const toastMessage = () => `
-<aside class="toast toast--show" id="default-toast">
-    <header class="toast__header">
+<aside class="toast toast--show" hidden aria-label="Notification" aria-live="polite">
+    <div class="toast__header">
         <h3>User Privacy Preferences</h3>
         <button id="default-toast-btn-hide" class="icon-btn" type="button" aria-label="Close toast">
             <svg class="icon icon--close" focusable="false" height="24" width="24">
                 <use xlink:href="#icon-close"></use>
             </svg>
         </button>
-    </header>
-    <p class="toast__body">
+    </div>
+    <div class="toast__main">
         We detected something unusual about a recent sign-in to your eBay account.
         To help keep you safe, we recommend you change the password.
-    </p>
-    <section class="toast__actions">
+    </div>
+    <div class="toast__footer">
         <button class="btn btn--delete">Reject</button>
         <button class="btn btn--secondary">Accept</button>
-    </section>
+    </div>
 </aside>
 `;
