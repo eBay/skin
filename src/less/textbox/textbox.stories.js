d8e96211427d8a8ebc2825c8da15eba4a832bab3
Andrew Gliga <agliga@ebay.com>
2020-06-18 15:21:22 -0700

    Added storybook examples for two icons and rtl fix

--- src/less/textbox/textbox.stories.js
+++ src/less/textbox/textbox.stories.js
@@ -30,6 +30,32 @@ export const iconEnd = () => `
 </span>
 `;
 
+export const iconDual = () => `
+<span class="textbox textbox--icon-end">
+    <svg class="icon icon--search" focusable="false" width="16" height="16" aria-hidden="true">
+        <use xlink:href="#icon-search"></use>
+    </svg>
+    <input class="textbox__control" type="text" placeholder="placeholder text" />
+    <svg class="icon icon--messages" focusable="false" width="16" height="16" aria-hidden="true">
+        <use xlink:href="#icon-messages"></use>
+    </svg>
+</span>
+`;
+
+export const iconDualActionable = () => `
+<span class="textbox textbox--icon-end">
+    <svg class="icon icon--search" focusable="false" width="16" height="16" aria-hidden="true">
+        <use xlink:href="#icon-search"></use>
+    </svg>
+    <input class="textbox__control" type="text" placeholder="placeholder text" />
+    <button class="icon-btn" type="button" aria-label="Choose Contact">
+        <svg class="icon icon--messages" focusable="false" width="16" height="16" aria-hidden="true">
+            <use xlink:href="#icon-messages"></use>
+        </svg>
+    </button>
+</span>
+`;
+
 export const Large = () => `
 <span class="textbox textbox--large">
     <input class="textbox__control" type="text" placeholder="placeholder text" />
@@ -108,6 +134,38 @@ export const iconEndRTL = () => `
 </div>
 `;
 
+export const iconBothRTL = () => `
+<div dir="rtl">
+    <span class="textbox textbox--icon-end">
+        <svg class="icon icon--search" focusable="false" width="16" height="16" aria-hidden="true">
+            <use xlink:href="#icon-search"></use>
+        </svg>
+
+        <input class="textbox__control" type="text" placeholder="placeholder text" />
+        <svg class="icon icon--messages" focusable="false" width="16" height="16" aria-hidden="true">
+            <use xlink:href="#icon-messages"></use>
+        </svg>
+    </span>
+</div>
+`;
+
+export const iconBothActionableRTL = () => `
+<div dir="rtl">
+    <span class="textbox textbox--icon-end">
+        <svg class="icon icon--search" focusable="false" width="16" height="16" aria-hidden="true">
+            <use xlink:href="#icon-search"></use>
+        </svg>
+
+        <input class="textbox__control" type="text" placeholder="placeholder text" />
+        <button class="icon-btn" type="button" aria-label="Choose Contact">
+            <svg class="icon icon--messages" focusable="false" width="16" height="16" aria-hidden="true">
+                <use xlink:href="#icon-messages"></use>
+            </svg>
+        </button>
+    </span>
+</div>
+`;
+
 export const actionableIcon = () => `
 <span class="textbox textbox--icon-end">
     <input class="textbox__control" type="text" placeholder="placeholder text" />
@@ -117,4 +175,17 @@ export const actionableIcon = () => `
         </svg>
     </button>
 </span>
+`;
+
+export const actionableIconRTL = () => `
+<div dir="rtl">
+    <span class="textbox textbox--icon-end">
+        <input class="textbox__control" type="text" placeholder="placeholder text" />
+        <button class="icon-btn" type="button" aria-label="Choose Contact">
+            <svg aria-hidden="true" class="icon icon--messages" focusable="false" width="16" height="16">
+                <use xlink:href="#icon-messages"></use>
+            </svg>
+        </button>
+    </span>
+</div>
 `;
