43c2c2c7bdfafd5bb82bb7d0d56025f4798f055e
Ian McBurnie <ianmcburnie@users.noreply.github.com>
2020-07-01 20:22:04 -0700

    Page Notice: removed heading around icon #1001

---
+++ src/less/page-notice/test.stories.js
@@ -0,0 +1,83 @@
+export default { title: 'Page Notice/Tests' };
+
+export const longText = () => `
+<section class="page-notice page-notice--confirmation" role="region" aria-labelledby="confirmation-status">
+    <div class="page-notice__header">
+        <svg aria-hidden="true" class="icon icon--confirmation-filled" focusable="false" height="24" width="24">
+            <use xlink:href="#icon-confirmation-filled"></use>
+        </svg>
+    </div>
+    <div class="page-notice__main">
+        <h3 class="page-notice__title">Your new listing is live:</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+</div>
+`;
+
+export const longTextAndLink = () => `
+<section class="page-notice page-notice--confirmation" role="region" aria-labelledby="confirmation-status">
+    <div class="page-notice__header">
+        <svg aria-hidden="true" class="icon icon--confirmation-filled" focusable="false" height="24" width="24">
+            <use xlink:href="#icon-confirmation-filled"></use>
+        </svg>
+    </div>
+    <div class="page-notice__main">
+        <h3 class="page-notice__title">Your new listing is live:</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p><a href="#">www.ebay.com</a></p>
+    </div>
+</div>
+`;
+
+export const multiParagraph = () => `
+<section class="page-notice page-notice--confirmation" role="region" aria-labelledby="confirmation-status">
+    <div class="page-notice__header">
+        <svg aria-hidden="true" class="icon icon--confirmation-filled" focusable="false" height="24" width="24">
+            <use xlink:href="#icon-confirmation-filled"></use>
+        </svg>
+    </div>
+    <div class="page-notice__main">
+        <h3 class="page-notice__title">Your new listing is live:</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+</div>
+`;
+
+export const multiParagraphWithLink = () => `
+<section class="page-notice page-notice--confirmation" role="region" aria-labelledby="confirmation-status">
+    <div class="page-notice__header">
+        <svg aria-hidden="true" class="icon icon--confirmation-filled" focusable="false" height="24" width="24">
+            <use xlink:href="#icon-confirmation-filled"></use>
+        </svg>
+    </div>
+    <div class="page-notice__main">
+        <h3 class="page-notice__title">Your new listing is live:</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p><a href="#">www.ebay.com</a></p>
+    </div>
+</div>
+`;
+
+export const noTitle = () => `
+<section class="page-notice page-notice--confirmation" role="region" aria-label="Confirmation">
+    <div class="page-notice__header">
+        <svg aria-hidden="true" class="icon icon--confirmation-filled" focusable="false" height="24" width="24">
+            <use xlink:href="#icon-confirmation-filled"></use>
+        </svg>
+    </div>
+    <div class="page-notice__main">
+        <p><strong>Congrats:</strong> You just listed <a href="#">"Spam and Eggs From the Cow's Perspective</a> (paperback)."</p>
+    </div>
+</section>
+`;
+
+export const noIcon = () => `
+<section class="page-notice page-notice--confirmation" role="region" aria-label="Confirmation">
+    <div class="page-notice__main">
+        <h3 class="page-notice__title">Your new listing is live:</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+</section>
+`;
