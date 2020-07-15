cbda8445a603b7c8ec9ab44c2c46ff391cb5b9ce
Andrew Gliga <agliga@ebay.com>
2020-07-15 12:08:52 -0700

    Added custom styles storybook file again (#1180)

--- scripts/storybook/copy-custom-styles.js
+++ scripts/storybook/copy-custom-styles.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const CUSTOM_STYLES_FILE = path.join(__dirname, '..', '..', 'docs', 'storybook', 'custom-styles.html');
+const CUSTOM_STYLES_FILE = path.join(__dirname, '..', '..', 'docs', 'custom-styles.html');
 const STORYBOOK_HEADER_FILE = path.join(__dirname, '..', '..', '.storybook', 'preview-head.html');
 
 const customStylesContent = fs.readFileSync(CUSTOM_STYLES_FILE, 'utf8');
