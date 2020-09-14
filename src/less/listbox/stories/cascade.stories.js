149d0a082b114786bb1bac7061c507e1150c5e20
Ian McBurnie <ianmcburnie@users.noreply.github.com>
2020-09-13 20:07:48 -0700

    Listbox: cleanup stories

--- src/less/listbox/stories/cascade.stories.js
+++ src/less/listbox/stories/cascade.stories.js
@@ -1,9 +1,34 @@
-export default { title: 'Listbox' };
+export default { title: 'Listbox/Cascade' };
 
-export const unselected = () => `
-<span class="listbox">
+export const RTL = () => `
+<span class="listbox" dir="rtl">
     <div class="listbox__options" role="listbox" tabindex="0">
-        <div class="listbox__option" role="option" aria-selected="false">
+        <div class="listbox__option" role="option" aria-selected="true">
+            <span class="listbox__value">Option 1</span>
+            <svg class="icon icon--tick-small" focusable="false" height="8" width="8">
+                <use xlink:href="#icon-tick-small"></use>
+            </svg>
+        </div>
+        <div class="listbox__option" role="option">
+            <span class="listbox__value">Option 2</span>
+            <svg class="icon icon--tick-small" focusable="false" height="8" width="8">
+                <use xlink:href="#icon-tick-small"></use>
+            </svg>
+        </div>
+        <div class="listbox__option" role="option">
+            <span class="listbox__value">Option 3</span>
+            <svg class="icon icon--tick-small" focusable="false" height="8" width="8">
+                <use xlink:href="#icon-tick-small"></use>
+            </svg>
+        </div>
+    </div>
+</span>
+`;
+
+export const color = () => `
+<span class="listbox">
+    <div class="listbox__options" role="listbox" style="color: red" tabindex="0">
+        <div class="listbox__option" role="option" aria-selected="true">
             <span class="listbox__value">Option 1</span>
             <svg class="icon icon--tick-small" focusable="false" height="8" width="8">
                 <use xlink:href="#icon-tick-small"></use>
@@ -25,9 +50,9 @@ export const unselected = () => `
 </span>
 `;
 
-export const selected = () => `
+export const fontSize = () => `
 <span class="listbox">
-    <div class="listbox__options" role="listbox" tabindex="0">
+    <div class="listbox__options" role="listbox" style="font-size: 200%" tabindex="0">
         <div class="listbox__option" role="option" aria-selected="true">
             <span class="listbox__value">Option 1</span>
             <svg class="icon icon--tick-small" focusable="false" height="8" width="8">
@@ -41,31 +66,6 @@ export const selected = () => `
             </svg>
         </div>
         <div class="listbox__option" role="option" aria-selected="false">
-            <span class="listbox__value">Option 3</span>
-            <svg class="icon icon--tick-small" focusable="false" height="8" width="8">
-                <use xlink:href="#icon-tick-small"></use>
-            </svg>
-        </div>
-    </div>
-</span>
-`;
-
-export const multiSelected = () => `
-<span class="listbox">
-    <div class="listbox__options" role="listbox" tabindex="0">
-        <div class="listbox__option" role="option" aria-selected="true">
-            <span class="listbox__value">Option 1</span>
-            <svg class="icon icon--tick-small" focusable="false" height="8" width="8">
-                <use xlink:href="#icon-tick-small"></use>
-            </svg>
-        </div>
-        <div class="listbox__option" role="option" aria-selected="true">
-            <span class="listbox__value">Option 2</span>
-            <svg class="icon icon--tick-small" focusable="false" height="8" width="8">
-                <use xlink:href="#icon-tick-small"></use>
-            </svg>
-        </div>
-        <div class="listbox__option" role="option" aria-selected="true">
             <span class="listbox__value">Option 3</span>
             <svg class="icon icon--tick-small" focusable="false" height="8" width="8">
                 <use xlink:href="#icon-tick-small"></use>
