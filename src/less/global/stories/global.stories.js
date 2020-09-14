cf5edb8008ead33803586d761a5bcfb6c17928bb
Ian McBurnie <ianmcburnie@users.noreply.github.com>
2020-09-13 22:10:24 -0700

    Ported form and icon alignment tests to storybook

--- src/less/global/stories/global.stories.js
+++ src/less/global/stories/global.stories.js
@@ -13,3 +13,107 @@ export const headings = () => `
     <hr/>
     <h6>This is a level 6 heading</h6>
 `;
+
+export const alignment = () => `
+<span class="textbox">
+    <input aria-label="Email Address" class="textbox__control" type="text" placeholder="placeholder text" />
+</span>
+
+<span class="select">
+    <select>
+        <option value="1">Option 1</option>
+        <option value="2">Option 2</option>
+        <option value="3">Option 3</option>
+    </select>
+    <svg class="icon icon--dropdown" focusable="false" height="8" width="8" aria-hidden="true">
+        <use xlink:href="#icon-dropdown"></use>
+    </svg>
+</span>
+
+<button class="btn btn--fixed-height">Button</button>
+
+<a href="http://www.ebay.com" class="fake-btn fake-btn--fixed-height">Link</a>
+
+<span class="combobox">
+    <span class="combobox__control">
+        <input name="combobox-default" placeholder="Combobox" role="combobox" type="text" aria-expanded="false" aria-haspopup="listbox" />
+        <svg class="icon icon--dropdown" focusable="false" height="8" width="8" aria-hidden="true">
+            <use xlink:href="#icon-dropdown"></use>
+        </svg>
+    </span>
+</span>
+
+<span class="listbox-button">
+    <button class="expand-btn expand-btn--fixed-height" aria-expanded="false" aria-haspopup="listbox">
+        <span class="expand-btn__cell">
+            <span class="expand-btn__text">Options</span>
+            <svg class="icon icon--dropdown" focusable="false" height="8" width="8" aria-hidden="true">
+                <use xlink:href="#icon-dropdown"></use>
+            </svg>
+        </span>
+    </button>
+</span>
+
+<span class="menu-button">
+    <button class="expand-btn expand-btn--fixed-height" aria-haspopup="true" type="button">
+        <span class="expand-btn__cell">
+            <span class="expand-btn__text">Menu</span>
+            <svg class="icon icon--dropdown" focusable="false" height="8" width="8" aria-hidden="true">
+                <use xlink:href="#icon-dropdown"></use>
+            </svg>
+        </span>
+    </button>
+</span>
+`;
+
+export const alignmentLarge = () => `
+<span class="textbox textbox--large">
+    <input aria-label="Email Address" class="textbox__control" type="text" placeholder="placeholder text" />
+</span>
+
+<span class="select select--large">
+    <select>
+        <option value="1">Option 1</option>
+        <option value="2">Option 2</option>
+        <option value="3">Option 3</option>
+    </select>
+    <svg class="icon icon--dropdown" focusable="false" height="8" width="8" aria-hidden="true">
+        <use xlink:href="#icon-dropdown"></use>
+    </svg>
+</span>
+
+<button class="btn btn--large btn--large-fixed-height">Button</button>
+
+<a href="http://www.ebay.com" class="fake-btn fake-btn--large fake-btn--large-fixed-height">Link</a>
+
+<span class="combobox combobox--large">
+    <span class="combobox__control">
+        <input name="combobox-default" placeholder="Combobox" role="combobox" type="text" aria-expanded="false" aria-haspopup="listbox" />
+        <svg class="icon icon--dropdown" focusable="false" height="8" width="8" aria-hidden="true">
+            <use xlink:href="#icon-dropdown"></use>
+        </svg>
+    </span>
+</span>
+
+<span class="listbox-button">
+    <button class="expand-btn expand-btn--large expand-btn--large-fixed-height" aria-expanded="false" aria-haspopup="listbox">
+        <span class="expand-btn__cell">
+            <span class="expand-btn__text">Options</span>
+            <svg class="icon icon--dropdown" focusable="false" height="8" width="8" aria-hidden="true">
+                <use xlink:href="#icon-dropdown"></use>
+            </svg>
+        </span>
+    </button>
+</span>
+
+<span class="menu-button">
+    <button class="expand-btn expand-btn--large expand-btn--large-fixed-height" aria-haspopup="true" type="button">
+        <span class="expand-btn__cell">
+            <span class="expand-btn__text">Menu</span>
+            <svg class="icon icon--dropdown" focusable="false" height="8" width="8" aria-hidden="true">
+                <use xlink:href="#icon-dropdown"></use>
+            </svg>
+        </span>
+    </button>
+</span>
+`;
