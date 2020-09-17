1cb12a06fbd9565523c6f47b83942e4e9f751079
Ian McBurnie <38065+ianmcburnie@users.noreply.github.com>
2020-09-17 09:38:07 -0700

    Expand-Button: deprecate primary/secondary (#1250)

--- src/less/expand-button/stories/deprecated/primary.stories.js
+++ src/less/expand-button/stories/deprecated/primary.stories.js
@@ -1,7 +1,7 @@
-export default { title: 'Expand Button/Priority/Secondary' };
+export default { title: 'Expand Button/Deprecated/Primary' };
 
 export const active = () => `
-<button type="button" class="expand-btn expand-btn--secondary">
+<button type="button" class="expand-btn expand-btn--primary">
     <span class="expand-btn__cell">
         <span>Button</span>
         <svg class="icon icon--dropdown" focusable="false" height="8" width="8" aria-hidden="true">
@@ -12,7 +12,7 @@ export const active = () => `
 `;
 
 export const disabled = () => `
-<button type="button" class="expand-btn expand-btn--secondary" disabled>
+<button type="button" class="expand-btn expand-btn--primary" disabled>
     <span class="expand-btn__cell">
         <span>Button</span>
         <svg class="icon icon--dropdown" focusable="false" height="8" width="8" aria-hidden="true">
--- src/less/expand-button/stories/deprecated/secondary.stories.js
+++ src/less/expand-button/stories/deprecated/secondary.stories.js
@@ -1,7 +1,7 @@
-export default { title: 'Expand Button/Priority/Primary' };
+export default { title: 'Expand Button/Deprecated/Secondary' };
 
 export const active = () => `
-<button type="button" class="expand-btn expand-btn--primary">
+<button type="button" class="expand-btn expand-btn--secondary">
     <span class="expand-btn__cell">
         <span>Button</span>
         <svg class="icon icon--dropdown" focusable="false" height="8" width="8" aria-hidden="true">
@@ -12,7 +12,7 @@ export const active = () => `
 `;
 
 export const disabled = () => `
-<button type="button" class="expand-btn expand-btn--primary" disabled>
+<button type="button" class="expand-btn expand-btn--secondary" disabled>
     <span class="expand-btn__cell">
         <span>Button</span>
         <svg class="icon icon--dropdown" focusable="false" height="8" width="8" aria-hidden="true">
