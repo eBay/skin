e599c965aa7d8d129e663d24cb4e68fa3dc81eb3
austingardner <gardneraustin@protonmail.com>
2020-12-08 09:55:32 -0800

    1281 image treatment (#1295)

--- src/less/carousel/stories/carousel.stories.js
+++ src/less/carousel/stories/carousel.stories.js
@@ -29,6 +29,35 @@ export const continuous = () => `
 </div>
 `;
 
+export const imageTreatment = () => `
+<div class="carousel">
+    <div class="carousel__container">
+        <button class="carousel__control carousel__control--prev" aria-label="Previous Slide - Top Products">
+            <svg aria-hidden="true" class="icon icon--carousel-prev" focusable="false">
+                <use xlink:href="#icon-carousel-prev"></use>
+            </svg>
+        </button>
+        <div class="carousel__viewport carousel__viewport--mask">
+            <ul class="carousel__list carousel__list--image-treatment carousel__list--image-treatment-demo">
+                <li><img src="http://ir.ebaystatic.com/cr/v/c1/skin/image-treatment/aztec-pyramid.jpeg" /></li>
+                <li><img src="http://ir.ebaystatic.com/cr/v/c1/skin/image-treatment/falls.jpeg" /></li>
+                <li><img src="http://ir.ebaystatic.com/cr/v/c1/skin/image-treatment/mountain.jpeg" /></li>
+                <li><img src="http://ir.ebaystatic.com/cr/v/c1/skin/image-treatment/shoes.jpeg"/></li>
+                <li><img src="http://ir.ebaystatic.com/cr/v/c1/skin/image-treatment/tall-cat.jpeg" /></li>
+                <li><img src="http://ir.ebaystatic.com/cr/v/c1/skin/image-treatment/wide-cat.jpeg"/></li>
+                <li>Card 7</li>
+                <li>Card 8</li>
+            </ul>
+        </div>
+        <button class="carousel__control carousel__control--next" aria-label="Next Slide - Top Products">
+            <svg aria-hidden="true" class="icon icon--carousel-next" focusable="false">
+                <use xlink:href="#icon-carousel-next"></use>
+            </svg>
+        </button>
+    </div>
+</div>
+`;
+
 export const slides = () => `
 <div class="carousel carousel--slides">
     <div class="carousel__container">
