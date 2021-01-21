ae1ea7c806218601c4eabdadeeb717a96bebf798
Andrew Gliga <agliga@ebay.com>
2021-01-21 14:32:55 -0800

    Updated stepper to DS v3.1.1

--- src/less/stepper/stories/stepper.stories.js
+++ src/less/stepper/stories/stepper.stories.js
@@ -6,37 +6,45 @@ export const base = () => `
         <div class="stepper__item stepper__item--confirmation" role="listitem">
             <span class="stepper__icon">
                 <svg aria-hidden="true" class="icon" focusable="false" height="24" width="24">
-                    <use xlink:href="#icon-confirmation-filled"></use>
+                    <use xlink:href="#icon-stepper-confirmation"></use>
                 </svg>
             </span>
-            <span class="stepper__text">Started</span>
+            <span class="stepper__text">
+                <h4>Started</h4>
+                <p>July 3rd</p>
+            </span>
         </div>
         <hr class="stepper__separator" role="presentation" />
         <div class="stepper__item stepper__item--confirmation" role="listitem">
             <span class="stepper__icon">
                 <svg aria-hidden="true" class="icon" focusable="false" height="24" width="24">
-                    <use xlink:href="#icon-confirmation-filled"></use>
+                    <use xlink:href="#icon-stepper-confirmation"></use>
                 </svg>
             </span>
-            <span class="stepper__text">Shipped</span>
+            <span class="stepper__text">
+                <h4>Shipped</h4>
+                <p>July 4th</p>
+            </span>
         </div>
         <hr class="stepper__separator" role="presentation" />
         <div aria-current="step" class="stepper__item stepper__item--current" role="listitem">
             <span class="stepper__icon">
-                <svg aria-hidden="true" class="icon" focusable="false" height="24" width="24">
-                    <use xlink:href="#icon-circle"></use>
-                </svg>
+                <span class="step" role="img" aria-label="3">3</span>
             </span>
-            <span class="stepper__text">Transit</span>
+            <span class="stepper__text">
+                <h4>Transit</h4>
+                <p>July 5th</p>
+            </span>
         </div>
         <hr class="stepper__separator" role="presentation" />
         <div class="stepper__item stepper__item--upcoming" role="listitem">
             <span class="stepper__icon">
-                <svg aria-hidden="true" class="icon" focusable="false" height="24" width="24">
-                    <use xlink:href="#icon-circle"></use>
-                </svg>
+                <span class="step" role="img" aria-label="4">4</span>
             </span>
-            <span class="stepper__text">Delivered</span>
+            <span class="stepper__text">
+                <h4>Delivered</h4>
+                <p>July 6th</p>
+            </span>
         </div>
     </div>
 </div>
@@ -48,40 +56,49 @@ export const RTL = () => `
         <div class="stepper__item stepper__item--confirmation" role="listitem">
             <span class="stepper__icon">
                 <svg aria-hidden="true" class="icon" focusable="false" height="24" width="24">
-                    <use xlink:href="#icon-confirmation-filled"></use>
+                    <use xlink:href="#icon-stepper-confirmation"></use>
                 </svg>
             </span>
-            <span class="stepper__text">Started</span>
+            <span class="stepper__text">
+                <h4>Started</h4>
+                <p>July 3rd</p>
+            </span>
         </div>
         <hr class="stepper__separator" role="presentation" />
         <div class="stepper__item stepper__item--confirmation" role="listitem">
             <span class="stepper__icon">
                 <svg aria-hidden="true" class="icon" focusable="false" height="24" width="24">
-                    <use xlink:href="#icon-confirmation-filled"></use>
+                    <use xlink:href="#icon-stepper-confirmation"></use>
                 </svg>
             </span>
-            <span class="stepper__text">Shipped</span>
+            <span class="stepper__text">
+                <h4>Shipped</h4>
+                <p>July 4th</p>
+            </span>
         </div>
         <hr class="stepper__separator" role="presentation" />
         <div aria-current="step" class="stepper__item stepper__item--current" role="listitem">
             <span class="stepper__icon">
-                <svg aria-hidden="true" class="icon" focusable="false" height="24" width="24">
-                    <use xlink:href="#icon-circle"></use>
-                </svg>
+                <span class="step" role="img" aria-label="3">3</span>
             </span>
-            <span class="stepper__text">Transit</span>
+            <span class="stepper__text">
+                <h4>Transit</h4>
+                <p>July 5th</p>
+            </span>
         </div>
         <hr class="stepper__separator" role="presentation" />
         <div class="stepper__item stepper__item--upcoming" role="listitem">
             <span class="stepper__icon">
-                <svg aria-hidden="true" class="icon" focusable="false" height="24" width="24">
-                    <use xlink:href="#icon-circle"></use>
-                </svg>
+                <span class="step" role="img" aria-label="4">4</span>
             </span>
-            <span class="stepper__text">Delivered</span>
+            <span class="stepper__text">
+                <h4>Delivered</h4>
+                <p>July 6th</p>
+            </span>
         </div>
     </div>
 </div>
+
 `;
 
 export const vertical = () => `
@@ -90,7 +107,7 @@ export const vertical = () => `
         <div class="stepper__item stepper__item--confirmation" role="listitem">
             <span class="stepper__icon">
                 <svg aria-hidden="true" class="icon" focusable="false" height="24" width="24">
-                    <use xlink:href="#icon-confirmation-filled"></use>
+                    <use xlink:href="#icon-stepper-confirmation"></use>
                 </svg>
             </span>
             <span class="stepper__text">
@@ -126,3 +143,82 @@ export const vertical = () => `
     </div>
 </div>
 `;
+
+
+export const infoState = () => `
+<div class="stepper">
+    <div class="stepper__items" role="list">
+        <div class="stepper__item stepper__item--confirmation" role="listitem">
+            <span class="stepper__icon">
+                <svg aria-hidden="true" class="icon" focusable="false" height="24" width="24">
+                    <use xlink:href="#icon-stepper-confirmation"></use>
+                </svg>
+            </span>
+            <span class="stepper__text">
+                <h4>Started</h4>
+                <p>July 3rd</p>
+            </span>
+        </div>
+        <hr class="stepper__separator" role="presentation" />
+            <div aria-current="step" class="stepper__item stepper__item--information stepper__item--transition-upcoming" role="listitem">
+                <span class="stepper__icon">
+                    <svg aria-hidden="true" class="icon" focusable="false" height="24" width="24">
+                        <use xlink:href="#icon-stepper-information"></use>
+                    </svg>
+                </span>
+                <span class="stepper__text">
+                    <h4>On Hold</h4>
+                    <p>July 5th</p>
+                </span>
+            </div>
+        <hr class="stepper__separator" role="presentation" />
+        <div class="stepper__item stepper__item--upcoming" role="listitem">
+            <span class="stepper__icon">
+                <span class="step" role="img" aria-label="4">4</span>
+            </span>
+            <span class="stepper__text">
+                <h4>Delivered</h4>
+                <p>July 6th</p>
+            </span>
+        </div>
+    </div>
+</div>`;
+
+export const errorState = () => `
+<div class="stepper">
+    <div class="stepper__items" role="list">
+        <div class="stepper__item stepper__item--confirmation" role="listitem">
+            <span class="stepper__icon">
+                <svg aria-hidden="true" class="icon" focusable="false" height="24" width="24">
+                    <use xlink:href="#icon-stepper-confirmation"></use>
+                </svg>
+            </span>
+            <span class="stepper__text">
+                <h4>Started</h4>
+                <p>July 3rd</p>
+            </span>
+        </div>
+        <hr class="stepper__separator" role="presentation" />
+        <div aria-current="step" class="stepper__item stepper__item--attention stepper__item--transition-upcoming" role="listitem">
+            <span class="stepper__icon">
+                <svg aria-hidden="true" class="icon" focusable="false" height="24" width="24">
+                    <use xlink:href="#icon-stepper-attention"></use>
+                </svg>
+            </span>
+            <span class="stepper__text">
+                <h4>Blocked</h4>
+                <p>July 5th</p>
+            </span>
+        </div>
+        <hr class="stepper__separator" role="presentation" />
+        <div class="stepper__item stepper__item--upcoming" role="listitem">
+            <span class="stepper__icon">
+                <span class="step" role="img" aria-label="4">4</span>
+            </span>
+            <span class="stepper__text">
+                <h4>Delivered</h4>
+                <p>July 6th</p>
+            </span>
+        </div>
+    </div>
+</div>`;
