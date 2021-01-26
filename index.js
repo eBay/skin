8c2cbb5cc1a46dbc56bad6ec34b239f76c225b3c
Andrew Gliga <agliga@ebay.com>
2021-01-26 09:41:30 -0800

    Updated stepper to DS v3.1.1 (#1336)

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ require('./menu');
 require('./page-notice');
 require('./pagination');
 require('./panel-dialog');
+require('./progress-stepper');
 require('./radio');
 require('./section-notice');
 require('./select');
