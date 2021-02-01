ff5d439cf7fcc18ded6030a4c6c9b34ba2657c93
Austin Gardner <gardneraustin@protonmail.com>
2021-02-01 11:45:49 -0800

    class instead of styles applied to all progress tags

--- src/less/progress-bar/stories/progress-bar.stories.js
+++ src/less/progress-bar/stories/progress-bar.stories.js
@@ -1,6 +1,7 @@
 export default { title: 'Progress Bar/Progress Bar' };
 
-export const base = () => `<progress style="margin: 25px;" value=40 min=0 max=100>0%</progress>`;
+export const base = () =>
+    `<progress class="progress" style="margin: 25px;" value=40 min=0 max=100>0%</progress>`;
 
 // export const Basic = () => <Button />
 base.storyName = 'Base';
