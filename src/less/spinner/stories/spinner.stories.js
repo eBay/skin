3ddd7f896f2da8be26257909bb912c922a6f623c
austingardner <gardneraustin@protonmail.com>
2021-02-19 15:56:35 -0700

    style(progress-spinner): updated to DS v3.1.1

--- src/less/spinner/stories/spinner.stories.js
+++
@@ -1,9 +0,0 @@
-export default { title: 'Spinner/Spinner' };
-
-export const base = () => '<span class="spinner" aria-label="Busy animation" role="img"></span>';
-
-export const RTL = () =>
-    '<div dir="rtl"><span class="spinner" aria-label="Busy animation" role="img"></span></div>';
-
-export const large = () =>
-    '<span class="spinner spinner--large" aria-label="Busy" role="img"></span>';
