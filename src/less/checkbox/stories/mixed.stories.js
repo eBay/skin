b3aad396cd5e6fa10d0c03a6849bf6a63c971d09
Ian McBurnie <ianmcburnie@users.noreply.github.com>
2021-03-12 14:47:34 -0800

    feat(checkbox): mixed checkbox #1280

---
+++ src/less/checkbox/stories/mixed.stories.js
@@ -0,0 +1,15 @@
+export default { title: 'Checkbox/Checkbox/Mixed' };
+
+export const base = () => `
+<span class="checkbox">
+    <input aria-checked="mixed" aria-label="Mixed checkbox" class="checkbox__control" type="checkbox" checked />
+    <span class="checkbox__icon" hidden>
+        <svg aria-hidden="true" class="checkbox__unchecked" focusable="false">
+            <use xlink:href="#icon-checkbox-unchecked"></use>
+        </svg>
+        <svg aria-hidden="true" class="checkbox__checked" focusable="false">
+            <use xlink:href="#icon-checkbox-mixed"></use>
+        </svg>
+    </span>
+</span>
+`;
