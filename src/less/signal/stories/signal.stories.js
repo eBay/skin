7cc1cfe62b1ded28d9042c640c0e77d2724274d4
Austin Gardner <gardneraustin@protonmail.com>
2021-03-31 07:51:33 -1000

    feat: signal

---
+++ src/less/signal/stories/signal.stories.js
@@ -0,0 +1,49 @@
+export default { title: 'Hotness Signals/Hotness Signals' };
+
+export const trustWorthy = () => `
+<span class="signal signal--trustworthy">
+    Trustworthy
+</span>
+`;
+
+export const recent = () => `
+<span class="signal signal--recent">
+    Recent
+</span>
+`;
+
+export const timeSensitive = () => `
+<span class="signal signal--time-sensitive">
+    Time Sensitive
+</span>
+`;
+
+export const neutral = () => `
+<span class="signal signal--neutral">
+    Neutral
+</span>
+`;
+
+export const trustWorthyDiv = () => `
+<div class="signal signal--trustworthy">
+    Trustworthy
+</div>
+`;
+
+export const recentDiv = () => `
+<div class="signal signal--recent">
+    Recent
+</div>
+`;
+
+export const timeSensitiveDiv = () => `
+<div class="signal signal--time-sensitive">
+    Time Sensitive
+</div>
+`;
+
+export const neutralDiv = () => `
+<div class="signal signal--neutral">
+    Neutral
+</div>
+`;
