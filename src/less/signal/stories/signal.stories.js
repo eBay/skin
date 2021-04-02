715d24d7f3ec00c1fe8bb1829e20adbe041d9a0a
austingardner <gardneraustin@protonmail.com>
2021-04-02 09:20:02 -1000

    feat: signal (aka hotness signals) (#1393)

---
+++ src/less/signal/stories/signal.stories.js
@@ -0,0 +1,25 @@
+export default { title: 'Hotness Signals/Hotness Signals' };
+
+export const trustWorthy = () => `
+<span class="signal signal--trustworthy">
+    Trustworthy
+</span>
+`;
+
+export const recent = () => `
+<span class="signal signal--recent">
+    Recent
+</span>
+`;
+
+export const timeSensitive = () => `
+<span class="signal signal--time-sensitive">
+    Time Sensitive
+</span>
+`;
+
+export const neutral = () => `
+<span class="signal signal--neutral">
+    Neutral
+</span>
+`;
