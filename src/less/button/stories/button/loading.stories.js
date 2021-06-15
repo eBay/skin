b05b313efbe2ec2101a12a9aeeb2c2668eccaeca
Austin Gardner <gardneraustin@protonmail.com>
2021-06-15 16:16:52 -0700

    feat(button): added ds evo styles for button, icon button, and badge

---
+++ src/less/button/stories/button/loading.stories.js
@@ -0,0 +1,33 @@
+export default { title: 'Button/Button/Loading' };
+
+export const loadingPrimary = () => `
+    <button class="btn btn--primary btn--loading">
+        <span class="btn__cell">
+        <span class="progress-spinner">
+        </span>
+    </button>
+`;
+
+export const loadingSecondary = () => `
+    <button class="btn btn--secondary btn--loading">
+        <span class="btn__cell">
+        <span class="progress-spinner">
+        </span>
+    </button>
+`;
+
+export const loadingTertiary = () => `
+    <button class="btn btn--tertiary btn--loading">
+        <span class="btn__cell">
+        <span class="progress-spinner">
+        </span>
+    </button>
+`;
+
+export const loadingLargePrimary = () => `
+<button class="btn btn--primary btn--large btn--loading">
+    <span class="btn__cell">
+        <span class="progress-spinner">
+    </span>
+</button>
+`;
