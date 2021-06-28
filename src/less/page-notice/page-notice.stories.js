3dec26bea71bbffde7cd1fdea1911b624315635b
austingardner <gardneraustin@protonmail.com>
2021-06-28 11:53:40 -0700

    feat(notices): evo design system updates (#1469)
    Co-authored-by: Ian McBurnie <ianmcburnie@users.noreply.github.com>

--- src/less/page-notice/page-notice.stories.js
+++ src/less/page-notice/page-notice.stories.js
@@ -8,11 +8,28 @@ export const confirmation = () => `
         </svg>
     </div>
     <div class="page-notice__main">
-        <h3 class="page-notice__title">Your new listing is live:</h3>
-        <p><a href="#">Spam and Eggs From the Cow's Perspective</a> (paperback).</p>
+        <h2 class="page-notice__title">You have opted into eBay Pay and </h2>
+        <p>will now get paid directly.</p>
     </div>
     <div class="page-notice__footer">
-        <a href="https://www.ebay.com" class="fake-btn fake-btn--secondary fake-btn--transparent">Action</a>
+        <button class="fake-link">Dismiss</button>
+    </div>
+</section>
+`;
+
+export const confirmationLink = () => `
+<section class="page-notice page-notice--confirmation" role="region" aria-label="Confirmation">
+    <div class="page-notice__header">
+        <svg class="icon icon--confirmation-filled" focusable="false" height="24" width="24" role="img" aria-label="Confirmation">
+            <use xlink:href="#icon-confirmation-filled"></use>
+        </svg>
+    </div>
+    <div class="page-notice__main">
+        <h2 class="page-notice__title">You have opted into eBay Pay and </h2>
+        <p>will now get paid directly.</p>
+    </div>
+    <div class="page-notice__footer">
+        <a href="https://ebay.com">Learn More</a>
     </div>
 </section>
 `;
@@ -26,11 +43,12 @@ export const RTL = () => `
             </svg>
         </div>
         <div class="page-notice__main">
-            <h3 class="page-notice__title">:Your new listing is live</h3>
+            <h2 class="page-notice__title">:Your new listing is live</h2>
             <p><a href="#">Spam and Eggs From the Cow's Perspective</a> (paperback)</p>
         </div>
+
         <div class="page-notice__footer">
-            <a href="https://www.ebay.com" class="fake-btn fake-btn--secondary fake-btn--transparent">Action</a>
+            <button class="fake-link">Dismiss</button>
         </div>
     </section>
 <div>
@@ -44,11 +62,28 @@ export const attention = () => `
         </svg>
     </div>
     <div class="page-notice__main">
-        <h3 class="page-notice__title">Please check the following details:</h3>
+        <h2 class="page-notice__title">Please check the following details:</h2>
         <p><a href="#">Card number</a>, <a href="#">Expiration date</a> &amp; <a href="#">Security code</a>.</p>
     </div>
     <div class="page-notice__footer">
-        <a href="https://www.ebay.com" class="fake-btn fake-btn--secondary fake-btn--transparent">Action</a>
+        <button class="fake-link">Learn More</button>
+    </div>
+</section>
+`;
+
+export const attentionLink = () => `
+<section class="page-notice page-notice--attention" role="region" aria-label="Attention">
+    <div class="page-notice__header">
+        <svg class="icon icon--attention-filled" focusable="false" height="24" width="24" role="img" aria-label="Attention">
+            <use xlink:href="#icon-attention-filled"></use>
+        </svg>
+    </div>
+    <div class="page-notice__main">
+        <h2 class="page-notice__title">Please check the following details:</h2>
+        <p><a href="#">Card number</a>, <a href="#">Expiration date</a> &amp; <a href="#">Security code</a>.</p>
+    </div>
+    <div class="page-notice__footer">
+        <a href="https://ebay.com">Learn More</a>
     </div>
 </section>
 `;
@@ -65,41 +100,16 @@ export const information = () => `
         <p><a href="#">Learn more</a>.</p>
     </div>
     <div class="page-notice__footer">
-        <a href="https://www.ebay.com" class="fake-btn fake-btn--secondary fake-btn--transparent">Action</a>
+        <button class="fake-link">Opt In</button>
     </div>
 </section>
 `;
 
-export const celebration = () => `
-<section class="page-notice page-notice--celebration" role="region" aria-label="Success">
-    <div class="page-notice__header">
-        <svg class="icon icon--confirmation-filled" focusable="false" height="24" width="24" role="img" aria-label="Success">
-            <use xlink:href="#icon-confirmation-filled"></use>
-        </svg>
-    </div>
+export const general = () => `
+<section class="page-notice page-notice--general" role="region" aria-label="Information">
     <div class="page-notice__main">
-        <h2 class="page-notice__title">Your order's in!</h2>
-        <p>We'll email updates to jonsnow@gmail.com. You should get it by Thu, Sept 22.</p>
-    </div>
-    <div class="page-notice__footer">
-        <a href="https://www.ebay.com" class="fake-btn fake-btn--secondary fake-btn--transparent">Action</a>
-    </div>
-</section>
-`;
-
-export const buttonCta = () => `
-<section class="page-notice page-notice--confirmation" role="region" aria-label="Confirmation">
-    <div class="page-notice__header">
-        <svg class="icon icon--confirmation-filled" focusable="false" height="24" width="24" role="img" aria-label="Confirmation">
-            <use xlink:href="#icon-confirmation-filled"></use>
-        </svg>
-    </div>
-    <div class="page-notice__main">
-        <h3 class="page-notice__title">Your new listing is live:</h3>
-        <p><a href="#">Spam and Eggs From the Cow's Perspective</a> (paperback).</p>
-    </div>
-    <div class="page-notice__footer">
-        <button class="btn btn--secondary btn--transparent">Action</button>
+        <h2 class="page-notice__title">We've updated the look and feel of this page.</h2>
+        <p>Customize anytime in settings.</p>
     </div>
 </section>
 `;
@@ -112,7 +122,7 @@ export const noFooter = () => `
         </svg>
     </div>
     <div class="page-notice__main">
-        <h3 class="page-notice__title">Your new listing is live:</h3>
+        <h2 class="page-notice__title">Your new listing is live:</h2>
         <p><a href="#">Spam and Eggs From the Cow's Perspective</a> (paperback).</p>
     </div>
 </section>
