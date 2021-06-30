9905cf63092b55c44c69cf4bd929cffab0ead86f
Ian McBurnie <38065+ianmcburnie@users.noreply.github.com>
2021-06-30 10:56:06 -0700

    feat(notices): rework headings (#1497)

--- src/less/section-notice/section-notice.stories.js
+++ src/less/section-notice/section-notice.stories.js
@@ -1,6 +1,14 @@
 export default { title: 'Section Notice/Section Notice' };
 
-export const base = () => `
+export const general = () => `
+<div class="section-notice" role="region">
+    <div class="section-notice__main">
+        <h3 class="section-notice__title">Your price has been updated.</h3>
+    </div>
+</div>
+`;
+
+export const generalWithButton = () => `
 <div class="section-notice" role="region">
     <div class="section-notice__main">
         <h3 class="section-notice__title">Your price has been updated.</h3>
@@ -11,7 +19,18 @@ export const base = () => `
 </div>
 `;
 
-export const confirmation = () => `
+export const generalWithLink = () => `
+<div class="section-notice" role="region">
+    <div class="section-notice__main">
+        <h3 class="section-notice__title">Your price has been updated.</h3>
+    </div>
+    <div class="section-notice__footer">
+        <a href="https://ebay.com">Learn More</a>
+    </div>
+</div>
+`;
+
+export const confirmationWithButton = () => `
 <div class="section-notice section-notice--confirmation" role="region">
     <div class="section-notice__header" role="region" aria-roledescription="Notice">
         <svg aria-hidden="true" focusable="false" class="icon--confirmation-filled">
@@ -27,7 +46,7 @@ export const confirmation = () => `
 </div>
 `;
 
-export const confirmationLink = () => `
+export const confirmationWithLink = () => `
 <div class="section-notice section-notice--confirmation" role="region">
     <div class="section-notice__header" role="region" aria-roledescription="Notice">
         <svg aria-hidden="true" focusable="false" class="icon--confirmation-filled">
@@ -43,25 +62,7 @@ export const confirmationLink = () => `
 </div>
 `;
 
-export const RTL = () => `
-<div dir="rtl">
-    <div class="section-notice section-notice--confirmation" role="region">
-        <div class="section-notice__header" role="region" aria-roledescription="Notice">
-            <svg aria-hidden="true" focusable="false" class="icon--confirmation-filled">
-                <use xlink:href="#icon-confirmation-filled"></use>
-            </svg>
-        </div>
-        <div class="section-notice__main">
-            <h3 class="section-notice__title">Your price has been updated</h3>
-        </div>
-        <div class="section-notice__footer">
-            <button class="fake-link">Dismiss</button>
-        </div>
-    </div>
-</div>
-`;
-
-export const attention = () => `
+export const attentionWithButton = () => `
 <div class="section-notice section-notice--attention" role="region">
     <div class="section-notice__header" role="region" aria-roledescription="Notice">
         <svg aria-hidden="true" focusable="false" class="icon--attention-filled">
@@ -72,12 +73,28 @@ export const attention = () => `
         <h3 class="section-notice__title">Add required aspects to keep item live.</h3>
     </div>
     <div class="section-notice__footer">
-        <button class="fake-link">Add</button>
+        <button class="fake-link">Dismiss</button>
     </div>
 </div>
 `;
 
-export const information = () => `
+export const attentionWithLink = () => `
+<div class="section-notice section-notice--attention" role="region">
+    <div class="section-notice__header" role="region" aria-roledescription="Notice">
+        <svg aria-hidden="true" focusable="false" class="icon--attention-filled">
+            <use xlink:href="#icon-attention-filled"></use>
+        </svg>
+    </div>
+    <div class="section-notice__main">
+        <h3 class="section-notice__title">Add required aspects to keep item live.</h3>
+    </div>
+    <div class="section-notice__footer">
+        <a href="https://ebay.com">Learn More</a>
+    </div>
+</div>
+`;
+
+export const informationWithButton = () => `
 <div class="section-notice section-notice--information" role="region">
     <div class="section-notice__header" role="region" aria-roledescription="Notice">
         <svg aria-hidden="true" focusable="false" class="icon--information-filled">
@@ -88,12 +105,12 @@ export const information = () => `
         <h3 class="section-notice__title">Send offers to interested buyers.</h3>
     </div>
     <div class="section-notice__footer">
-        <button class="fake-link">Send Offers</button>
+        <button class="fake-link">Dismiss</button>
     </div>
 </div>
 `;
 
-export const informationLink = () => `
+export const informationWithLink = () => `
 <div class="section-notice section-notice--information" role="region">
     <div class="section-notice__header" role="region" aria-roledescription="Notice">
         <svg aria-hidden="true" focusable="false" class="icon--information-filled">
@@ -105,50 +122,6 @@ export const informationLink = () => `
     </div>
     <div class="section-notice__footer">
         <a href="https://ebay.com">Send Offers</a>
-    </div>
-</div>
-`;
-
-export const noFooter = () => `
-<div class="section-notice section-notice--confirmation" role="region">
-    <div class="section-notice__header" role="region" aria-roledescription="Notice">
-        <svg aria-hidden="true" focusable="false" class="icon--confirmation-filled">
-            <use xlink:href="#icon-confirmation-filled"></use>
-        </svg>
-    </div>
-    <div class="section-notice__main">
-        <h3 class="section-notice__title">Your price has been updated.</h3>
-    </div>
-</div>
-`;
-
-export const longText = () => `
-<div class="section-notice section-notice--confirmation" role="region">
-    <div class="section-notice__header" role="region" aria-roledescription="Notice">
-        <svg aria-hidden="true" focusable="false" class="icon--confirmation-filled">
-            <use xlink:href="#icon-confirmation-filled"></use>
-        </svg>
-    </div>
-    <div class="section-notice__main">
-        <h3 class="section-notice__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h3>
-    </div>
-</div>
-`;
-
-export const buttonCta = () => `
-<div class="section-notice section-notice--confirmation" role="region">
-    <div class="section-notice__header" role="region" aria-roledescription="Notice">
-        <svg role="img" focusable="false" class="icon--attention-filled">
-            <title>Comfirmation</title>
-            <use xlink:href="#icon-confirmation-filled"></use>
-        </svg>
-    </div>
-    <span class="section-notice__main">
-        <h3 class="section-notice__title">Congrats! You are the highest bidder!</h3>
-        <p>Here's a second line</p>
-    </span>
-    <div class="section-notice__footer">
-        <button class="fake-link">Dismiss</button>
     </div>
 </div>
 `;
