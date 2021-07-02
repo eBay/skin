8c2840149aa3fd8ff952170413ced3907e827432
austingardner <gardneraustin@protonmail.com>
2021-07-01 17:23:05 -0700

    fix(textbox): vertically centered icon button with large textbox (#1499)

--- src/less/textbox/stories/icon.stories.js
+++ src/less/textbox/stories/icon.stories.js
@@ -54,3 +54,28 @@ export const dualActionable = () => `
     </button>
 </span>
 `;
+
+export const actionableLarge = () => `
+<span class="textbox textbox--icon-end">
+    <input class="textbox__control textbox__control--large" type="text" placeholder="placeholder text" />
+    <button class="icon-btn" type="button" aria-label="Choose Contact">
+        <svg aria-hidden="true" class="icon icon--messages" focusable="false" width="16" height="16">
+            <use xlink:href="#icon-messages"></use>
+        </svg>
+    </button>
+</span>
+`;
+
+export const dualActionableLarge = () => `
+<span class="textbox textbox--icon-end">
+    <svg class="icon icon--search" focusable="false" width="16" height="16" aria-hidden="true">
+        <use xlink:href="#icon-search"></use>
+    </svg>
+    <input class="textbox__control textbox__control--large" type="text" placeholder="placeholder text" />
+    <button class="icon-btn" type="button" aria-label="Choose Contact">
+        <svg class="icon icon--messages" focusable="false" width="16" height="16" aria-hidden="true">
+            <use xlink:href="#icon-messages"></use>
+        </svg>
+    </button>
+</span>
+`;
