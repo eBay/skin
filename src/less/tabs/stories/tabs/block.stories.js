9b9ba9e18382451db823a5337425f4783af3b443
Ian McBurnie <38065+ianmcburnie@users.noreply.github.com>
2021-07-27 14:43:51 -0700

    feat(tabs): evo updates (#1523)

--- src/less/tabs/stories/tabs/block.stories.js
+++ src/less/tabs/stories/tabs/block.stories.js
@@ -54,27 +54,29 @@ export const four = () => `
 `;
 
 export const RTL = () => `
-<div class="tabs" dir="rtl">
-    <div class="tabs__items" role="tablist">
-        <div aria-controls="panel1" class="tabs__item" role="tab" tabindex="0" aria-selected="true" id="tab1"><span>Tab 1</span></div>
-        <div aria-controls="panel2" class="tabs__item" role="tab" tabindex="-1" aria-selected="false" id="tab2"><span>Tab 2</span></div>
-        <div aria-controls="panel3" class="tabs__item" role="tab" tabindex="-1" aria-selected="false" id="tab3"><span>Tab 3</span></div>
-        <div aria-controls="panel4" class="tabs__item" role="tab" tabindex="-1" aria-selected="false" id="tab4"><span>Tab 4</span></div>
-    </div>
-    <div class="tabs__content">
-        <div id="panel1" role="tabpanel" aria-labelledby="tab1"><div class="tabs__cell"><p>.Lorem ipsum dolor sit amet</p></div></div>
-        <div id="panel2" role="tabpanel" hidden aria-labelledby="tab2"><div class="tabs__cell"><p>.Lorem ipsum dolor sit amet</p></div></div>
-        <div id="panel3" role="tabpanel" hidden aria-labelledby="tab3"><div class="tabs__cell"><p>.Lorem ipsum dolor sit amet</p></div></div>
-        <div id="panel4" role="tabpanel" hidden aria-labelledby="tab4"><div class="tabs__cell"><p>.Lorem ipsum dolor sit amet</p></div></div>
+<div dir="rtl">
+    <div class="tabs">
+        <div class="tabs__items" role="tablist">
+            <div aria-controls="panel1" class="tabs__item" role="tab" tabindex="0" aria-selected="true" id="tab1"><span>Tab 1</span></div>
+            <div aria-controls="panel2" class="tabs__item" role="tab" tabindex="-1" aria-selected="false" id="tab2"><span>Tab 2</span></div>
+            <div aria-controls="panel3" class="tabs__item" role="tab" tabindex="-1" aria-selected="false" id="tab3"><span>Tab 3</span></div>
+            <div aria-controls="panel4" class="tabs__item" role="tab" tabindex="-1" aria-selected="false" id="tab4"><span>Tab 4</span></div>
+        </div>
+        <div class="tabs__content">
+            <div id="panel1" role="tabpanel" aria-labelledby="tab1"><div class="tabs__cell"><p>.Lorem ipsum dolor sit amet</p></div></div>
+            <div id="panel2" role="tabpanel" hidden aria-labelledby="tab2"><div class="tabs__cell"><p>.Lorem ipsum dolor sit amet</p></div></div>
+            <div id="panel3" role="tabpanel" hidden aria-labelledby="tab3"><div class="tabs__cell"><p>.Lorem ipsum dolor sit amet</p></div></div>
+            <div id="panel4" role="tabpanel" hidden aria-labelledby="tab4"><div class="tabs__cell"><p>.Lorem ipsum dolor sit amet</p></div></div>
+        </div>
     </div>
 </div>
 `;
 
 export const longText = () => `
-<div class="tabs" style="max-width: 600px;">
+<div class="tabs">
     <div class="tabs__items" role="tablist">
         <div aria-controls="default-tabpanel-1" aria-selected="true" class="tabs__item" id="default-tab-1" role="tab" tabindex="0">
-            <span>Tab 1 with lots of text that could easily take up several lines</span>
+            <span>Tab 1 with lots of text that will not truncate or wrap</span>
         </div>
         <div aria-controls="default-tabpanel-2" aria-selected="false" class="tabs__item" id="default-tab-2" role="tab" tabindex="-1">
             <span>Tab 2</span>
