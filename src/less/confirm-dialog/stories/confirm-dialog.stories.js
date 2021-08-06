4d9cae3db0f1fae11504ff7b562da5889ed8ac0a
Ian McBurnie <38065+ianmcburnie@users.noreply.github.com>
2021-08-06 15:56:01 -0700

    feat(lightbox-dialog): split out alert & confirm (#1531)

---
+++ src/less/confirm-dialog/stories/confirm-dialog.stories.js
@@ -0,0 +1,18 @@
+export default { title: 'Confirm Dialog/Confirm Dialog' };
+
+export const base = () => `
+<div aria-labelledby="confirm-dialog-title" aria-modal="true" class="confirm-dialog confirm-dialog--mask-fade" role="dialog">
+    <div class="confirm-dialog__window confirm-dialog__window--fade">
+        <div class="confirm-dialog__header">
+            <h2 id="confirm-dialog-title" class="confirm-dialog__title">Delete Address?</h2>
+        </div>
+        <div class="confirm-dialog__main">
+            <p id="confirm-dialog__description">You will permanently lose this address.</p>
+        </div>
+        <div class="confirm-dialog__footer">
+            <button class="btn confirm-dialog__reject">Cancel</button>
+            <button class="btn btn--primary confirm-dialog__confirm" aria-describedby="confirm-dialog__description">Delete</button>
+        </div>
+    </div>
+</div>
+`;
