99a819ca122553b5c73dd310a28053b473ef3fbc
Andrew Gliga <agliga@ebay.com>
2022-02-22 10:50:56 -0800

    fix(drawer-dialog): added footer back (#1677)

--- src/less/drawer-dialog/stories/drawer-dialog.stories.js
+++ src/less/drawer-dialog/stories/drawer-dialog.stories.js
@@ -41,6 +41,33 @@ export const partial = () => `
 </div>
 `;
 
+export const withFooter = () => `
+<div aria-labelledby="drawer-dialog-title" aria-modal="true" class="drawer-dialog drawer-dialog--mask-fade-slow" role="dialog">
+    <div class="drawer-dialog__window drawer-dialog__window--slide">
+        <button class="drawer-dialog__handle"></button>
+        <div class="drawer-dialog__header">
+            <h2 id="dialog-title" class="large-text-1 bold-text">Heading</h2>
+            <button aria-label="Close dialog" class="icon-btn drawer-dialog__close" type="button">
+                <svg aria-hidden="true" class="icon icon--close" focusable="false" height="16" width="16">
+                    <use xlink:href="#icon-close"></use>
+                </svg>
+            </button>
+        </div>
+        <div class="drawer-dialog__main">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+            <p><a href="http://www.ebay.com">www.ebay.com</a></p>
+        </div>
+        <div class="drawer-dialog__footer">
+            <button class="btn">Cancel</button>
+            <button class="btn btn--primary">Submit</button>
+        </div>
+    </div>
+</div>
+`;
+
 export const RTL = () => `
 <div dir="rtl">
     <div aria-labelledby="drawer-dialog-title" aria-modal="true" class="drawer-dialog drawer-dialog--mask-fade-slow" role="dialog">
