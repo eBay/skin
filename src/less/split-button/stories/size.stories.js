49b25f2b443a226fd3ef677bfd51a570ec8cc4b8
Andrew Gliga <agliga@ebay.com>
2022-03-01 17:21:07 -0800

    chore: added stories for split button

---
+++ src/less/split-button/stories/size.stories.js
@@ -0,0 +1,60 @@
+export default { title: 'Split Button/Split Button/Size' };
+export const longTextCollapsed = () => `
+<span class="split-button">
+    <button class="btn btn--primary btn--split-start btn--truncated" aria-haspopup="true" type="button">
+        <span class="btn__cell">
+            <span class="btn__text">Longest Button Text Example Lorem Ipsum Dolor</span>
+        </span>
+    </button>
+    <span class="menu-button">
+        <button class="btn btn--primary btn--split-end" aria-haspopup="true" type="button">
+            <svg class="icon icon--dropdown" focusable="false" height="10" width="14" aria-hidden="true">
+                <use xlink:href="#icon-dropdown"></use>
+            </svg>
+        </button>
+        <div class="menu-button__menu">
+            <div class="menu-button__items" role="menu">
+                <div class="menu-button__item" role="menuitem">
+                    <span>Item 10000</span>
+                </div>
+                <div class="menu-button__item" role="menuitem">
+                    <span>Item 20000</span>
+                </div>
+                <div class="menu-button__item" role="menuitem">
+                    <span>Item 30000</span>
+                </div>
+            </div>
+        </div>
+    </span>
+</span>
+`;
+
+export const longTextExpanded = () => `
+<span class="split-button">
+    <button class="btn btn--primary btn--split-start btn--truncated" aria-haspopup="true" type="button">
+        <span class="btn__cell">
+            <span class="btn__text">Longest Button Text Example Lorem Ipsum Dolor</span>
+        </span>
+    </button>
+    <span class="menu-button">
+        <button class="btn btn--primary btn--split-end" aria-expanded="true" aria-haspopup="true" type="button">
+            <svg class="icon icon--dropdown" focusable="false" height="10" width="14" aria-hidden="true">
+                <use xlink:href="#icon-dropdown"></use>
+            </svg>
+        </button>
+        <div class="menu-button__menu">
+            <div class="menu-button__items" role="menu">
+                <div class="menu-button__item" role="menuitem">
+                    <span>Item 10000</span>
+                </div>
+                <div class="menu-button__item" role="menuitem">
+                    <span>Item 20000</span>
+                </div>
+                <div class="menu-button__item" role="menuitem">
+                    <span>Item 30000</span>
+                </div>
+            </div>
+        </div>
+    </span>
+</span>
+`;
