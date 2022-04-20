f0a78103d3894312ac8748318e54a890a5156785
Arthur Khachatryan <arkhachatryan@ebay.com>
2022-04-20 11:29:57 -0700

    fix(star-rating-select): fixed markup in story, reduced unnecessary classes, fixed BEM

--- src/less/star-rating-select/star-rating-select.stories.js
+++ src/less/star-rating-select/star-rating-select.stories.js
@@ -1,47 +1,47 @@
 export default { title: 'Star Rating Select/Star Rating Select' };
 
 export const base = () => `
-<div class="star-rating-select">
-    <span class="radio">
-        <input aria-label="1 Star" class="radio__control" type="radio" name="radio-star-rating-select" value="1">
-        <span class="radio__icon">
-            <svg class="star-rating-select__icon" focusable="false">
+<div role="radiogroup" aria-label="Rate this product" class="star-rating-select">
+    <span class="star-rating-select__radio">
+        <input aria-label="1 Star" type="radio" name="radio-star-rating-select" value="1">
+        <span class="star-rating-select__radio__icon">
+            <svg focusable="false">
                 <use xlink:href="#icon-star-dynamic"></use>
             </svg>
         </span>
     </span>
 
-    <span class="radio">
-        <input aria-label="2 Stars" class="radio__control" type="radio" name="radio-star-rating-select" value="2">
-        <span class="radio__icon">
-            <svg class="star-rating-select__icon" focusable="false">
+    <span class="star-rating-select__radio">
+        <input aria-label="2 Stars" type="radio" name="radio-star-rating-select" value="2">
+        <span class="star-rating-select__radio__icon">
+            <svg focusable="false">
                 <use xlink:href="#icon-star-dynamic"></use>
             </svg>
         </span>
     </span>
 
-    <span class="radio">
-        <input aria-label="2 Stars" class="radio__control" type="radio" name="radio-star-rating-select" value="3">
-        <span class="radio__icon">
-            <svg class="star-rating-select__icon" focusable="false">
+    <span class="star-rating-select__radio">
+        <input aria-label="3 Stars" type="radio" name="radio-star-rating-select" value="3">
+        <span class="star-rating-select__radio__icon">
+            <svg focusable="false">
                 <use xlink:href="#icon-star-dynamic"></use>
             </svg>
         </span>
     </span>
 
-    <span class="radio">
-        <input aria-label="2 Stars" class="radio__control" type="radio" name="radio-star-rating-select" value="4">
-        <span class="radio__icon">
-            <svg class="star-rating-select__icon" focusable="false">
+    <span class="star-rating-select__radio">
+        <input aria-label="4 Stars" type="radio" name="radio-star-rating-select" value="4">
+        <span class="star-rating-select__radio__icon">
+            <svg focusable="false">
                 <use xlink:href="#icon-star-dynamic"></use>
             </svg>
         </span>
     </span>
 
-    <span class="radio">
-        <input aria-label="2 Stars" class="radio__control" type="radio" name="radio-star-rating-select" value="5">
-        <span class="radio__icon">
-            <svg class="star-rating-select__icon" focusable="false">
+    <span class="star-rating-select__radio">
+        <input aria-label="5 Stars" type="radio" name="radio-star-rating-select" value="5">
+        <span class="star-rating-select__radio__icon">
+            <svg focusable="false">
                 <use xlink:href="#icon-star-dynamic"></use>
             </svg>
         </span>
