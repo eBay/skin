776c05f549d23ebc7bd23dd220b0a8ac6c327e8b
Ian McBurnie <38065+ianmcburnie@users.noreply.github.com>
2022-04-22 12:07:17 -0700

    fix(docs): add aria-hidden to checkbox & radio SVG (#1731)

--- src/less/star-rating-select/star-rating-select.stories.js
+++ src/less/star-rating-select/star-rating-select.stories.js
@@ -5,7 +5,7 @@ export const base = () => `
     <span class="star-rating-select__radio">
         <input class="star-rating-select__control" aria-label="1 Star" type="radio" name="radio-star-rating-select" value="1">
         <span class="star-rating-select__radio-icon">
-            <svg focusable="false">
+            <svg focusable="false" height="12" width="12" aria-hidden="true">
                 <use xlink:href="#icon-star-dynamic"></use>
             </svg>
         </span>
@@ -14,7 +14,7 @@ export const base = () => `
     <span class="star-rating-select__radio">
         <input class="star-rating-select__control" aria-label="2 Stars" type="radio" name="radio-star-rating-select" value="2">
         <span class="star-rating-select__radio-icon">
-            <svg focusable="false">
+            <svg focusable="false" height="12" width="12" aria-hidden="true">
                 <use xlink:href="#icon-star-dynamic"></use>
             </svg>
         </span>
@@ -23,7 +23,7 @@ export const base = () => `
     <span class="star-rating-select__radio">
         <input class="star-rating-select__control" aria-label="3 Stars" type="radio" name="radio-star-rating-select" value="3">
         <span class="star-rating-select__radio-icon">
-            <svg focusable="false">
+            <svg focusable="false" height="12" width="12" aria-hidden="true">
                 <use xlink:href="#icon-star-dynamic"></use>
             </svg>
         </span>
@@ -32,7 +32,7 @@ export const base = () => `
     <span class="star-rating-select__radio">
         <input class="star-rating-select__control" aria-label="4 Stars" type="radio" name="radio-star-rating-select" value="4">
         <span class="star-rating-select__radio-icon">
-            <svg focusable="false">
+            <svg focusable="false" height="12" width="12" aria-hidden="true">
                 <use xlink:href="#icon-star-dynamic"></use>
             </svg>
         </span>
@@ -41,7 +41,7 @@ export const base = () => `
     <span class="star-rating-select__radio">
         <input class="star-rating-select__control" aria-label="5 Stars" type="radio" name="radio-star-rating-select" value="5">
         <span class="star-rating-select__radio-icon">
-            <svg focusable="false">
+            <svg focusable="false" height="12" width="12" aria-hidden="true">
                 <use xlink:href="#icon-star-dynamic"></use>
             </svg>
         </span>
