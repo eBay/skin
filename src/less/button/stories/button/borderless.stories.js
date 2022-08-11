85f3cb70e5d47702cc7270d13ec99d3979f4083f
Andrew Gliga <agliga@ebay.com>
2022-08-11 09:59:03 -0700

    fix(button): added styles for diabled borderless (#1835)

---
+++ src/less/button/stories/button/borderless.stories.js
@@ -0,0 +1,28 @@
+export default { title: 'Button/Borderless' };
+
+export const base = () => `<button class="btn btn--borderless">
+    <span class="btn__cell">
+        <span class="btn__text">Button</span>
+        <svg class="icon icon--dropdown" focusable="false" height="8" width="8" aria-hidden="true">
+            <use xlink:href="#icon-dropdown"></use>
+        </svg>
+    </span>
+</button>`;
+
+export const disabled = () => `<button class="btn btn--borderless" disabled="true">
+    <span class="btn__cell">
+        <span class="btn__text">Button</span>
+        <svg class="icon icon--dropdown" focusable="false" height="8" width="8" aria-hidden="true">
+            <use xlink:href="#icon-dropdown"></use>
+        </svg>
+    </span>
+</button>`;
+
+export const ariaDisabled = () => `<button class="btn btn--borderless" aria-disabled="true">
+    <span class="btn__cell">
+        <span class="btn__text">Button</span>
+        <svg class="icon icon--dropdown" focusable="false" height="8" width="8" aria-hidden="true">
+            <use xlink:href="#icon-dropdown"></use>
+        </svg>
+    </span>
+</button>`;
