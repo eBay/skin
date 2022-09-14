94c63c7fa12bc12ec66fa50140abc9097becb0e8
Arthur Khachatryan <arkhachatryan@ebay.com>
2022-09-14 09:01:06 -0700

    feat(section-title): updated section titles to match new specs (#1867)
    * feat(section-title): updated titles to match new specs

--- src/less/section-title/stories/rtl.stories.js
+++ src/less/section-title/stories/rtl.stories.js
@@ -23,31 +23,86 @@ export const subheading = () => `
 
 export const linked = () => `
 <div dir="rtl">
-    <div class="section-title">
+    <div class="section-title section-title">
         <div class="section-title__title-container">
-            <h2 class="section-title__title">
-                <a href="https://www.ebay.com">Today’s Deals – All With Free Shipping</a>
-                <svg class="icon icon--arrow-right-extra-small" focusable="false" height="10" width="10" aria-hidden="true">
-                    <use xlink:href="#icon-arrow-right-extra-small"></use>
-                </svg>
-            </h2>
+            <h2 class="section-title__title">Marketing Type Content Title</h2>
         </div>
+        <a class="section-title__cta" href="https://www.ebay.com">See all</a>
     </div>
 </div>
 `;
 
 export const linkedWithSubheading = () => `
 <div dir="rtl">
-    <div class="section-title">
+    <div class="section-title section-title">
         <div class="section-title__title-container">
-            <h2 class="section-title__title">
-                <a href="https://www.ebay.com">Today’s Deals – All With Free Shipping</a>
-                <svg class="icon icon--arrow-right-extra-small" focusable="false" height="10" width="10" aria-hidden="true">
-                    <use xlink:href="#icon-arrow-right-extra-small"></use>
-                </svg>
-            </h2>
+            <h2 class="section-title__title">Marketing Type Content Title</h2>
             <span class="section-title__subtitle">Recommended for you</span>
         </div>
+        <a class="section-title__cta" href="https://www.ebay.com">See all</a>
+    </div>
+</div>
+`;
+
+export const overflow = () => `
+<div dir="rtl">
+    <div class="section-title">
+        <div class="section-title__title-container">
+            <h2 class="section-title__title">Marketing Type Content Title</h2>
+        </div>
+        <div class="section-title__overflow">
+            <span class="menu-button">
+                <button class="menu-button__button icon-btn" type="button" aria-expanded="true" aria-haspopup="true" type="button">
+                    <svg class="icon icon--overflow" focusable="false" width="24" height="24" aria-hidden="true">
+                        <use href="#icon-overflow"></use>
+                    </svg>
+                </button>
+                <div class="menu-button__menu menu-button__menu--reverse">
+                    <div class="menu-button__items" role="menu">
+                        <div class="menu-button__item" role="menuitem"><span>Item 1</span></div>
+                        <div class="menu-button__item" role="menuitem"><span>Item 2</span></div>
+                        <div class="menu-button__item" role="menuitem"><span>Item 3</span></div>
+                    </div>
+                </div>
+            </span>
+        </div>
+    </div>
+</div>
+`;
+
+export const withFavorite = () => `
+<div dir="rtl">
+    <div class="section-title">
+        <div class="section-title__title-container">
+            <h2 class="section-title__title">Marketing Type Content Title</h2>
+            <span class="section-title__subtitle">Recommended for you</span>
+        </div>
+        <div class="section-title__info">
+            <button class="icon-btn infotip__host" type="button" aria-expanded="false" aria-label="Help">
+                <svg aria-hidden="true" class="icon icon--save-small" focusable="false" width="16" height="14">
+                    <use href="#icon-save-small"></use>
+                </svg>
+            </button>
+        </div>
+    </div>
+</div>
+`;
+
+export const withFavoriteAndLink = () => `
+<div dir="rtl">
+    <div class="section-title">
+        <div class="section-title__title-container">
+            <h2 class="section-title__title">Marketing Type Content Title</h2>
+            <span class="section-title__subtitle">Recommended for you</span>
+        </div>
+        <div class="section-title__info">
+            <button class="icon-btn infotip__host" type="button" aria-expanded="false" aria-label="Help">
+                <svg aria-hidden="true" class="icon icon--save-small" focusable="false" width="16" height="14">
+                    <use href="#icon-save-small"></use>
+                </svg>
+            </button>
+        </div>
+        <a class="section-title__cta" href="https://www.ebay.com">See all</a>
     </div>
 </div>
 `;
