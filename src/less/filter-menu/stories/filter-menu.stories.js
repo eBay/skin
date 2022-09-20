1906e03f354f68c39bfa2198bb83b83bd36167c4
LuLaValva <lukelavalva@gmail.com>
2022-09-20 10:53:37 -0700

    feat: simplified css and added story

--- src/less/filter-menu/stories/filter-menu.stories.js
+++ src/less/filter-menu/stories/filter-menu.stories.js
@@ -40,6 +40,46 @@ export const base = () => `
 </span>
 `;
 
+export const withDisabled = () => `
+<span class="filter-menu">
+    <div class="filter-menu__items" role="menu">
+        <div class="filter-menu__item" role="menuitemcheckbox" tabindex="0" aria-checked="false" aria-disabled="true">
+            <span class="filter-menu__checkbox">
+                <svg class="icon icon--checkbox-unchecked" focusable="false" height="18" width="18">
+                    <use xlink:href="#icon-checkbox-unchecked"></use>
+                </svg>
+                <svg class="icon icon--checkbox-checked" focusable="false" height="18" width="18">
+                    <use xlink:href="#icon-checkbox-checked"></use>
+                </svg>
+            </span>
+            <span class="filter-menu__text">Item 1 with a long string that will wrap to another line</span>
+        </div>
+        <div class="filter-menu__item" role="menuitemcheckbox" aria-checked="false">
+            <span class="filter-menu__checkbox">
+                <svg class="icon icon--checkbox-unchecked" focusable="false" height="18" width="18">
+                    <use xlink:href="#icon-checkbox-unchecked"></use>
+                </svg>
+                <svg class="icon icon--checkbox-checked" focusable="false" height="18" width="18">
+                    <use xlink:href="#icon-checkbox-checked"></use>
+                </svg>
+            </span>
+            <span class="filter-menu__text">Item 2</span>
+        </div>
+        <div class="filter-menu__item" role="menuitemcheckbox" aria-checked="true" aria-disabled="true">
+            <span class="filter-menu__checkbox">
+                <svg class="icon icon--checkbox-unchecked" focusable="false" height="18" width="18">
+                    <use xlink:href="#icon-checkbox-unchecked"></use>
+                </svg>
+                <svg class="icon icon--checkbox-checked" focusable="false" height="18" width="18">
+                    <use xlink:href="#icon-checkbox-checked"></use>
+                </svg>
+            </span>
+            <span class="filter-menu__text">Item 3</span>
+        </div>
+    </div>
+</span>
+`;
+
 export const withFooterButton = () => `
 <span class="filter-menu">
     <div class="filter-menu__items" role="menu">
