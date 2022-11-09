8494425726bd094c7fa1ad5df2bd70cbf1433d8c
Arthur Khachatryan <arkhachatryan@ebay.com>
2022-11-09 05:38:15 -0800

    chore(breadcrumbs): cleanup, forced some use cases, add stories

--- src/less/breadcrumbs/stories/button-overflow.stories.js
+++ src/less/breadcrumbs/stories/button-overflow.stories.js
@@ -1,5 +1,47 @@
 export default { title: 'Breadcrumbs/Buttons/Overflow' };
 
+export const minimum = () => `
+<nav aria-labelledby="breadcrumbs-heading" class="breadcrumbs breadcrumb--overflow" role="navigation">
+    <h2 id="breadcrumbs-heading" class="clipped">You are here</h2>
+    <ul>
+        <li>
+            <button>ebay</button>
+            <svg class="icon icon--breadcrumb" focusable="false" height="8" width="8" aria-hidden="true">
+                <use href="#icon-breadcrumb"></use>
+            </svg>
+        </li>
+        <li>
+            <span class="fake-menu-button">
+                <button class="fake-menu-button__button icon-btn" aria-expanded="false" aria-haspopup="true" aria-label="eBay Menu"  type="button">
+                    <svg aria-hidden="true" class="icon icon--overflow-small" focusable="false">
+                        <use href="#icon-overflow-small"></use>
+                    </svg>
+                </button>
+
+                <ul class="fake-menu-button__menu">
+                    <li>
+                        <a class="fake-menu-button__item" href="https://www.ebay.com/rpp/cell-phone-pda">Cell Phones, Smart Watches &amp; Accessories</a>
+                    </li>
+                    <li>
+                        <a class="fake-menu-button__item" href="https://www.ebay.com/b/Smart-Watch-Accessories/182064/bn_16565905">Smart Watch Accessories</a>
+                    </li>
+                    <li>
+                        <a class="fake-menu-button__item" href="#">Smart Watch Bands</a>
+                    </li>
+                </ul>
+            </span>
+
+            <svg class="icon icon--breadcrumb" focusable="false" height="8" width="8" aria-hidden="true">
+                <use href="#icon-breadcrumb"></use>
+            </svg>
+        </li>
+        <li>
+            <button aria-current="location">Current Band</button>
+        </li>
+    </ul>
+</nav>
+`;
+
 export const collapsed = () => `
 <nav aria-labelledby="breadcrumbs-heading" class="breadcrumbs breadcrumb--overflow" role="navigation">
     <h2 id="breadcrumbs-heading" class="clipped">You are here</h2>
@@ -36,13 +78,7 @@ export const collapsed = () => `
             </svg>
         </li>
         <li>
-            <button>Smart Watch Band Type 1</button>
-            <svg class="icon icon--breadcrumb" focusable="false" height="8" width="8" aria-hidden="true">
-                <use href="#icon-breadcrumb"></use>
-            </svg>
-        </li>
-        <li>
-            <button>Smart Watch Band Type 2</button>
+            <button>Smart Watch Band Long Band Text Type 2</button>
             <svg class="icon icon--breadcrumb" focusable="false" height="8" width="8" aria-hidden="true">
                 <use href="#icon-breadcrumb"></use>
             </svg>
@@ -89,7 +125,7 @@ export const expanded = () => `
             </svg>
         </li>
         <li>
-            <button>Smart Watch Band Type 1</button>
+            <button>Smart Watch Band Long Band Text Type 2</button>
             <svg class="icon icon--breadcrumb" focusable="false" height="8" width="8" aria-hidden="true">
                 <use href="#icon-breadcrumb"></use>
             </svg>
