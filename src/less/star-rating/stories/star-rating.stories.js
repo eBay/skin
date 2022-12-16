94586e6638fa78e7cf59da148b80f4b12961effe
Arthur Khachatryan <arkhachatryan@ebay.com>
2022-12-16 07:05:26 -0800

    fix(various-modules): fixed various prod issues, added missing stories

---
+++ src/less/star-rating/stories/star-rating.stories.js
@@ -0,0 +1,73 @@
+export default { title: 'Star Rating' };
+
+export const empty = () => `
+<div role="img" aria-label="Rating: 0/5" class="star-rating" data-stars="0">
+    <svg class="star-rating__icon" focusable="false" height="16" width="16" aria-hidden="true">
+        <use href="#icon-star-dynamic"></use>
+    </svg>
+
+    <svg class="star-rating__icon" focusable="false" height="16" width="16" aria-hidden="true">
+        <use href="#icon-star-dynamic"></use>
+    </svg>
+
+    <svg class="star-rating__icon" focusable="false" height="16" width="16" aria-hidden="true">
+        <use href="#icon-star-dynamic"></use>
+    </svg>
+
+    <svg class="star-rating__icon" focusable="false" height="16" width="16" aria-hidden="true">
+        <use href="#icon-star-dynamic"></use>
+    </svg>
+
+    <svg class="star-rating__icon" focusable="false" height="16" width="16" aria-hidden="true">
+        <use href="#icon-star-dynamic"></use>
+    </svg>
+</div>
+`;
+
+export const twoAndAHalfStars = () => `
+<div role="img" aria-label="Rating: 2.5/5" class="star-rating" data-stars="2-5">
+    <svg class="star-rating__icon" focusable="false" height="16" width="16" aria-hidden="true">
+        <use href="#icon-star-dynamic"></use>
+    </svg>
+
+    <svg class="star-rating__icon" focusable="false" height="16" width="16" aria-hidden="true">
+        <use href="#icon-star-dynamic"></use>
+    </svg>
+
+    <svg class="star-rating__icon" focusable="false" height="16" width="16" aria-hidden="true">
+        <use href="#icon-star-dynamic"></use>
+    </svg>
+
+    <svg class="star-rating__icon" focusable="false" height="16" width="16" aria-hidden="true">
+        <use href="#icon-star-dynamic"></use>
+    </svg>
+
+    <svg class="star-rating__icon" focusable="false" height="16" width="16" aria-hidden="true">
+        <use href="#icon-star-dynamic"></use>
+    </svg>
+</div>
+`;
+
+export const fourStars = () => `
+<div role="img" aria-label="Rating: 4/5" class="star-rating" data-stars="4">
+    <svg class="star-rating__icon" focusable="false" height="16" width="16" aria-hidden="true">
+        <use href="#icon-star-dynamic"></use>
+    </svg>
+
+    <svg class="star-rating__icon" focusable="false" height="16" width="16" aria-hidden="true">
+        <use href="#icon-star-dynamic"></use>
+    </svg>
+
+    <svg class="star-rating__icon" focusable="false" height="16" width="16" aria-hidden="true">
+        <use href="#icon-star-dynamic"></use>
+    </svg>
+
+    <svg class="star-rating__icon" focusable="false" height="16" width="16" aria-hidden="true">
+        <use href="#icon-star-dynamic"></use>
+    </svg>
+
+    <svg class="star-rating__icon" focusable="false" height="16" width="16" aria-hidden="true">
+        <use href="#icon-star-dynamic"></use>
+    </svg>
+</div>
+`;
