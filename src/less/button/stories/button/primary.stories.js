d11dce26577214cd3f838b73aadf466cf29c5fd0
Arthur Khachatryan <arkhachatryan@ebay.com>
2023-03-24 13:42:46 -0700

    fix(buttons): reverted primary disabled state handling (#2021)

--- src/less/button/stories/button/primary.stories.js
+++ src/less/button/stories/button/primary.stories.js
@@ -40,15 +40,3 @@ export const busy = () => `
     </span>
 </button>
 `;
-
-export const busyDisabled = () => `
-<button class="btn btn--primary" aria-label="Busy" aria-disabled="true" disabled>
-    <span class="btn__cell">
-        <span class="progress-spinner">
-            <svg class="icon icon--spinner-24" focusable="false" aria-hidden="true">
-                <use href="#icon-spinner-24"></use>
-            </svg>
-        </span>
-    </span>
-</button>
-`;
