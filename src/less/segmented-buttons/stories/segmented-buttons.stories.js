7126723150f80fb0da1aa25ed935056711f7a72c
Andrew Gliga <agliga4@gmail.com>
2023-05-10 11:51:13 -0700

    fix: review comments

--- src/less/segmented-buttons/stories/segmented-buttons.stories.js
+++ src/less/segmented-buttons/stories/segmented-buttons.stories.js
@@ -1,29 +1,29 @@
 export default { title: "Segmented Buttons" };
 export const base = () => `
     <div class="segmented-buttons">
-        <ul class="segmented-buttons__items">
-            <li class="segmented-buttons__item"><button type="button" class="segmented-buttons__button" aria-current="true">Day</button></li>
-            <li class="segmented-buttons__item"><button type="button" class="segmented-buttons__button">Month</button></li>
-            <li class="segmented-buttons__item"><button type="button" class="segmented-buttons__button">Year</button></li>
+        <ul>
+            <li><button type="button" class="segmented-buttons__button" aria-current="true">Day</button></li>
+            <li><button type="button" class="segmented-buttons__button">Month</button></li>
+            <li><button type="button" class="segmented-buttons__button">Year</button></li>
         </ul>
     </div>
 `;
 
 export const shortItems = () => `
     <div class="segmented-buttons">
-        <ul class="segmented-buttons__items">
-            <li class="segmented-buttons__item"><button type="button" class="segmented-buttons__button">Q1</button></li>
-            <li class="segmented-buttons__item"><button type="button" class="segmented-buttons__button" aria-current="true">Q2</button></li>
-            <li class="segmented-buttons__item"><button type="button" class="segmented-buttons__button">Q3</button></li>
-            <li class="segmented-buttons__item"><button type="button" class="segmented-buttons__button">Q4</button></li>
+        <ul>
+            <li><button type="button" class="segmented-buttons__button">Q1</button></li>
+            <li><button type="button" class="segmented-buttons__button" aria-current="true">Q2</button></li>
+            <li><button type="button" class="segmented-buttons__button">Q3</button></li>
+            <li><button type="button" class="segmented-buttons__button">Q4</button></li>
         </ul>
     </div>
 `;
 
 export const withIcons = () => `
     <div class="segmented-buttons">
-        <ul class="segmented-buttons__items">
-            <li class="segmented-buttons__item">
+        <ul>
+            <li>
                 <button type="button" aria-current="true" class="segmented-buttons__button">
                 <span class="segmented-buttons__button-cell">
                         <svg aria-hidden="true" class="icon icon--full-view-24" focusable="false" height="16" width="16">
@@ -33,7 +33,7 @@ export const withIcons = () => `
                     </span>
                 </button>
             </li>
-            <li class="segmented-buttons__item">
+            <li>
                 <button type="button" class="segmented-buttons__button">
                     <span class="segmented-buttons__button-cell">
                         <svg aria-hidden="true" class="icon icon--mobile-24" focusable="false" height="16" width="16">
@@ -50,11 +50,50 @@ export const withIcons = () => `
 export const inContainer = () => `
 <div style="width: 300px;">
     <div class="segmented-buttons">
-        <ul class="segmented-buttons__items">
-            <li class="segmented-buttons__item"><button type="button" class="segmented-buttons__button">Q1</button></li>
-            <li class="segmented-buttons__item"><button type="button" class="segmented-buttons__button">Q2</button></li>
-            <li class="segmented-buttons__item"><button type="button" class="segmented-buttons__button" aria-current="true">Q3</button></li>
-            <li class="segmented-buttons__item"><button type="button" class="segmented-buttons__button">Q4</button></li>
+        <ul>
+            <li><button type="button" class="segmented-buttons__button">Q1</button></li>
+            <li><button type="button" class="segmented-buttons__button">Q2</button></li>
+            <li><button type="button" class="segmented-buttons__button" aria-current="true">Q3</button></li>
+            <li><button type="button" class="segmented-buttons__button">Q4</button></li>
+        </ul>
+    </div>
+</div>
+`;
+
+export const inheritFontSize = () => `
+<div style="font-size: 300%">
+    <div class="segmented-buttons">
+        <ul>
+            <li><button type="button" class="segmented-buttons__button">Q1</button></li>
+            <li><button type="button" class="segmented-buttons__button">Q2</button></li>
+            <li><button type="button" class="segmented-buttons__button" aria-current="true">Q3</button></li>
+            <li><button type="button" class="segmented-buttons__button">Q4</button></li>
+        </ul>
+    </div>
+</div>
+`;
+
+export const inheritColors = () => `
+<div style="color:red;">
+    <div class="segmented-buttons">
+        <ul>
+            <li><button type="button" class="segmented-buttons__button">Q1</button></li>
+            <li><button type="button" class="segmented-buttons__button">Q2</button></li>
+            <li><button type="button" class="segmented-buttons__button" aria-current="true">Q3</button></li>
+            <li><button type="button" class="segmented-buttons__button">Q4</button></li>
+        </ul>
+    </div>
+</div>
+`;
+
+export const rtl = () => `
+<div dir="rtl">
+    <div class="segmented-buttons">
+        <ul>
+            <li><button type="button" class="segmented-buttons__button">Q1</button></li>
+            <li><button type="button" class="segmented-buttons__button">Q2</button></li>
+            <li><button type="button" class="segmented-buttons__button" aria-current="true">Q3</button></li>
+            <li><button type="button" class="segmented-buttons__button">Q4</button></li>
         </ul>
     </div>
 </div>
