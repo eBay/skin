e7207b2666f50877070e52c716a3f2fbd09f426f
Arthur Khachatryan <arkhachatryan@ebay.com>
2023-05-12 10:34:12 -0700

    feat(toggle-button): new component

---
+++ src/less/toggle-button/stories/toggle-button.stories.js
@@ -0,0 +1,17 @@
+export default { title: "Toggle Button" };
+
+export const toggledOff = () => `
+<button type="button" class="toggle-button" aria-pressed="false">
+    <span class="toggle-button__content">
+        <span class="toggle-button__title">Text Button Toggled Off</span>
+    </span>
+</button>
+`;
+
+export const toggledOn = () => `
+<button type="button" class="toggle-button" aria-pressed="true">
+    <span class="toggle-button__content">
+        <span class="toggle-button__title">Text Button Toggled On</span>
+    </span>
+</button>
+`;
