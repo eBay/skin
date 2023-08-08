13c20d9901eba8e9fcbe8cf821389497c2cf89fc
Arthur Khachatryan <arkhachatryan@ebay.com>
2023-08-08 13:23:41 -0700

    fix(button): add destructive red to borderless

---
+++ src/less/button/stories/destructive-button/borderless.stories.js
@@ -0,0 +1,4 @@
+export default { title: "Skin/Button/Destructive/Borderless" };
+
+export const base = () =>
+    '<button class="btn btn--borderless btn--destructive">Destructive Borderless Button</button>';
