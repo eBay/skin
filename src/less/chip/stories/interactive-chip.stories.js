187cda1e1eb7dda90278b1b8b2d7a9e1c7479fd3
Arthur Khachatryan <arkhachatryan@ebay.com>
2023-08-30 11:41:55 -0700

    fix(chip): a few fixes, added stories, etc

---
+++ src/less/chip/stories/interactive-chip.stories.js
@@ -0,0 +1,135 @@
+export default { title: "Skin/Chip/Interactive Chip" };
+
+export const typical = () => `
+<span class="chip">
+    <span id="chip-interactive-1-text" class="chip__text">Football</span>
+    <button class="chip__button" type="button" aria-label="Remove" aria-describedby="chip-interactive-1-text">
+        <svg class="icon icon--close-16" focusable="false" width="14" height="14" aria-hidden="true">
+            <use href="#icon-close-16"></use>
+        </svg>
+    </button>
+</span>
+`;
+
+export const leastContent = () => `
+<span class="chip">
+    <span id="chip-interactive-1-text" class="chip__text">I</span>
+    <button class="chip__button" type="button" aria-label="Remove" aria-describedby="chip-interactive-1-text">
+        <svg class="icon icon--close-16" focusable="false" width="14" height="14" aria-hidden="true">
+            <use href="#icon-close-16"></use>
+        </svg>
+    </button>
+</span>
+`;
+
+export const mostContent = () => `
+<span class="chip">
+    <span id="chip-interactive-1-text" class="chip__text">Chip with the longest potentially possible content</span>
+    <button class="chip__button" type="button" aria-label="Remove" aria-describedby="chip-interactive-1-text">
+        <svg class="icon icon--close-16" focusable="false" width="14" height="14" aria-hidden="true">
+            <use href="#icon-close-16"></use>
+        </svg>
+    </button>
+</span>
+`;
+
+export const set = () => `
+<span class="chip">
+    <span id="chip-interactive-1-text" class="chip__text">Football</span>
+    <button class="chip__button" type="button" aria-label="Remove" aria-describedby="chip-interactive-1-text">
+        <svg class="icon icon--close-16" focusable="false" width="14" height="14" aria-hidden="true">
+            <use href="#icon-close-16"></use>
+        </svg>
+    </button>
+</span>
+
+<span class="chip">
+    <span id="chip-interactive-2-text" class="chip__text">Basketball</span>
+    <button class="chip__button" type="button" aria-label="Remove" aria-describedby="chip-interactive-2-text">
+        <svg class="icon icon--close-16" focusable="false" width="14" height="14" aria-hidden="true">
+            <use href="#icon-close-16"></use>
+        </svg>
+    </button>
+</span>
+
+<span class="chip">
+    <span id="chip-interactive-3-text" class="chip__text">Baseball</span>
+    <button class="chip__button" type="button" aria-label="Remove" aria-describedby="chip-interactive-3-text">
+        <svg class="icon icon--close-16" focusable="false" width="14" height="14" aria-hidden="true">
+            <use href="#icon-close-16"></use>
+        </svg>
+    </button>
+</span>
+
+<span class="chip">
+    <span id="chip-interactive-4-text" class="chip__text">Hockey</span>
+    <button class="chip__button" type="button" aria-label="Remove" aria-describedby="chip-interactive-4-text">
+        <svg class="icon icon--close-16" focusable="false" width="14" height="14" aria-hidden="true">
+            <use href="#icon-close-16"></use>
+        </svg>
+    </button>
+</span>
+
+<span class="chip">
+    <span id="chip-interactive-5-text" class="chip__text">Tennis</span>
+    <button class="chip__button" type="button" aria-label="Remove" aria-describedby="chip-interactive-5-text">
+        <svg class="icon icon--close-16" focusable="false" width="14" height="14" aria-hidden="true">
+            <use href="#icon-close-16"></use>
+        </svg>
+    </button>
+</span>
+
+<span class="chip">
+    <span id="chip-interactive-6-text" class="chip__text">Golf</span>
+    <button class="chip__button" type="button" aria-label="Remove" aria-describedby="chip-interactive-6-text">
+        <svg class="icon icon--close-16" focusable="false" width="14" height="14" aria-hidden="true">
+            <use href="#icon-close-16"></use>
+        </svg>
+    </button>
+</span>
+
+<span class="chip">
+    <span id="chip-interactive-7-text" class="chip__text">Gymnastics</span>
+    <button class="chip__button" type="button" aria-label="Remove" aria-describedby="chip-interactive-7-text">
+        <svg class="icon icon--close-16" focusable="false" width="14" height="14" aria-hidden="true">
+            <use href="#icon-close-16"></use>
+        </svg>
+    </button>
+</span>
+
+<span class="chip">
+    <span id="chip-interactive-8-text" class="chip__text">Swimming</span>
+    <button class="chip__button" type="button" aria-label="Remove" aria-describedby="chip-interactive-8-text">
+        <svg class="icon icon--close-16" focusable="false" width="14" height="14" aria-hidden="true">
+            <use href="#icon-close-16"></use>
+        </svg>
+    </button>
+</span>
+
+<span class="chip">
+    <span id="chip-interactive-9-text" class="chip__text">Cricket</span>
+    <button class="chip__button" type="button" aria-label="Remove" aria-describedby="chip-interactive-9-text">
+        <svg class="icon icon--close-16" focusable="false" width="14" height="14" aria-hidden="true">
+            <use href="#icon-close-16"></use>
+        </svg>
+    </button>
+</span>
+
+<span class="chip">
+    <span id="chip-interactive-10-text" class="chip__text">Volleyball</span>
+    <button class="chip__button" type="button" aria-label="Remove" aria-describedby="chip-interactive-10-text">
+        <svg class="icon icon--close-16" focusable="false" width="14" height="14" aria-hidden="true">
+            <use href="#icon-close-16"></use>
+        </svg>
+    </button>
+</span>
+
+<span class="chip">
+    <span id="chip-interactive-11-text" class="chip__text">Rugby</span>
+    <button class="chip__button" type="button" aria-label="Remove" aria-describedby="chip-interactive-11-text">
+        <svg class="icon icon--close-16" focusable="false" width="14" height="14" aria-hidden="true">
+            <use href="#icon-close-16"></use>
+        </svg>
+    </button>
+</span>
+`;
---
+++ src/less/chip/stories/static-chip.stories.js
@@ -0,0 +1,65 @@
+export default { title: "Skin/Chip/Static Chip" };
+
+export const typical = () => `
+<span class="chip">
+    <span class="chip__text">Football</span>
+</span>
+`;
+
+export const leastContent = () => `
+<span class="chip">
+    <span class="chip__text">I</span>
+</span>
+`;
+
+export const mostContent = () => `
+<span class="chip">
+    <span class="chip__text">Chip with the longest potentially possible content</span>
+</span>
+`;
+
+export const set = () => `
+<span class="chip">
+    <span class="chip__text">Football</span>
+</span>
+
+<span class="chip">
+    <span class="chip__text">Basketball</span>
+</span>
+
+<span class="chip">
+    <span class="chip__text">Baseball</span>
+</span>
+
+<span class="chip">
+    <span class="chip__text">Hockey</span>
+</span>
+
+<span class="chip">
+    <span class="chip__text">Tennis</span>
+</span>
+
+<span class="chip">
+    <span class="chip__text">Golf</span>
+</span>
+
+<span class="chip">
+    <span class="chip__text">Gymnastics</span>
+</span>
+
+<span class="chip">
+    <span class="chip__text">Swimming</span>
+</span>
+
+<span class="chip">
+    <span class="chip__text">Cricket</span>
+</span>
+
+<span class="chip">
+    <span class="chip__text">Volleyball</span>
+</span>
+
+<span class="chip">
+    <span class="chip__text">Rugby</span>
+</span>
+`;
