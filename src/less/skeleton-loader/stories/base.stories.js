73c56a6835f4960044fedb4f7374cb5eb490b575
Goutham Ponnada <sponnada@ebay.com>
2023-09-10 20:59:53 -0700

    feat(skeleton-loader): new module

---
+++ src/less/skeleton-loader/stories/base.stories.js
@@ -0,0 +1,22 @@
+export default { title: "Skin/Skeleton Loader/Base" };
+
+export const avatar = () =>
+    '<div class="skeleton skeleton--avatar" aria-hidden="true"></div>';
+
+export const buttonSmall = () =>
+    '<div class="skeleton skeleton--button-small" aria-hidden="true"></div>';
+
+export const buttonLarge = () =>
+    '<div class="skeleton skeleton--button-large" aria-hidden="true"></div>';
+
+export const textSmall = () =>
+    '<div class="skeleton skeleton--text-small" aria-hidden="true"></div>';
+
+export const textLarge = () =>
+    '<div class="skeleton skeleton--text-large" aria-hidden="true"></div>';
+
+export const textSmallTwoRow = () =>
+    '<div class="skeleton skeleton--text-small skeleton--text-two-row" aria-hidden="true"></div>';
+
+export const textLargeTwoRow = () =>
+    '<div class="skeleton skeleton--text-large skeleton--text-two-row" aria-hidden="true"></div>';
