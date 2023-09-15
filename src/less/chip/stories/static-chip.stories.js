e73190861f73732faae6bdf2dc5a504fdf434fee
Arthur Khachatryan <arkhachatryan@ebay.com>
2023-09-15 14:26:29 -0700

    feat(chip): new module (#2141)

---
+++ src/less/chip/stories/static-chip.stories.js
@@ -0,0 +1,155 @@
+export default { title: "Skin/Chip/Static Chip" };
+
+export const typical = () => `
+<span class="chip">
+    <span class="chip__text">Football</span>
+</span>
+`;
+
+export const leastContent = () => `
+<span class="chip">
+    <span class="chip__text">I</span>
+</span>
+`;
+
+export const mostContent = () => `
+<span class="chip">
+    <span class="chip__text">Chip with the longest potentially possible content</span>
+</span>
+`;
+
+export const set = () => `
+<span class="chip">
+    <span class="chip__text">Football</span>
+</span>
+
+<span class="chip">
+    <span class="chip__text">Basketball</span>
+</span>
+
+<span class="chip">
+    <span class="chip__text">Baseball</span>
+</span>
+
+<span class="chip">
+    <span class="chip__text">Hockey</span>
+</span>
+
+<span class="chip">
+    <span class="chip__text">Tennis</span>
+</span>
+
+<span class="chip">
+    <span class="chip__text">Golf</span>
+</span>
+
+<span class="chip">
+    <span class="chip__text">Gymnastics</span>
+</span>
+
+<span class="chip">
+    <span class="chip__text">Swimming</span>
+</span>
+
+<span class="chip">
+    <span class="chip__text">Cricket</span>
+</span>
+
+<span class="chip">
+    <span class="chip__text">Volleyball</span>
+</span>
+
+<span class="chip">
+    <span class="chip__text">Rugby</span>
+</span>
+`;
+
+export const list = () => `
+<ul>
+    <li>
+        <span class="chip">
+            <span class="chip__text">Football</span>
+        </span>
+    </li>
+
+    <li>
+        <span class="chip">
+            <span class="chip__text">Basketball</span>
+        </span>
+    </li>
+
+    <li>
+        <span class="chip">
+            <span class="chip__text">Baseball</span>
+        </span>
+    </li>
+
+    <li>
+        <span class="chip">
+            <span class="chip__text">Hockey</span>
+        </span>
+    </li>
+
+    <li>
+        <span class="chip">
+            <span class="chip__text">Tennis</span>
+        </span>
+    </li>
+
+    <li>
+        <span class="chip">
+            <span class="chip__text">Golf</span>
+        </span>
+    </li>
+
+    <li>
+        <span class="chip">
+            <span class="chip__text">Gymnastics</span>
+        </span>
+    </li>
+
+    <li>
+        <span class="chip">
+            <span class="chip__text">Swimming</span>
+        </span>
+    </li>
+
+    <li>
+        <span class="chip">
+            <span class="chip__text">Cricket</span>
+        </span>
+    </li>
+
+    <li>
+        <span class="chip">
+            <span class="chip__text">Volleyball</span>
+        </span>
+    </li>
+
+    <li>
+        <span class="chip">
+            <span class="chip__text">Rugby</span>
+        </span>
+    </li>
+</ul>
+`;
+
+export const largeText = () => `
+    <span class="chip" style="font-size: 200%;">
+        <span class="chip__text">Large Text</span>
+    </span>
+`;
+
+export const letterSpacing = () => `
+    <span class="chip" style="letter-spacing: calc(14px * 0.12);">
+        <span class="chip__text">Letter Spacing</span>
+    </span>
+`;
+
+export const RTL = () => `
+<div dir="rtl">
+    <span class="chip">
+        <span class="chip__text">Football RTL</span>
+    </span>
+</div>
+`;
