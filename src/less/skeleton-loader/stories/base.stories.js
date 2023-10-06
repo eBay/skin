4c72e813489335204b8998c2bca29bf1ab64d925
Goutham Ponnada <sponnada@ebay.com>
2023-10-06 12:30:11 -0700

    feat(skeleton-loader): added tile layout

--- src/less/skeleton-loader/stories/base.stories.js
+++ src/less/skeleton-loader/stories/base.stories.js
@@ -1,22 +1,39 @@
 export default { title: "Skin/Skeleton Loader/Base" };
 
 export const avatar = () =>
-    '<div class="skeleton skeleton--avatar" aria-hidden="true"></div>';
+    '<div role="img" class="skeleton skeleton--avatar" aria-hidden="true" aria-label="loading"></div>';
+
+export const button = () =>
+    '<div role="img" class="skeleton skeleton--button" aria-hidden="true" aria-label="loading"></div>';
 
 export const buttonSmall = () =>
-    '<div class="skeleton skeleton--button-small" aria-hidden="true"></div>';
+    '<div role="img" class="skeleton skeleton--button-small" aria-hidden="true" aria-label="loading"></div>';
 
 export const buttonLarge = () =>
-    '<div class="skeleton skeleton--button-large" aria-hidden="true"></div>';
+    '<div role="img" class="skeleton skeleton--button-large" aria-hidden="true" aria-label="loading"></div>';
 
 export const textSmall = () =>
-    '<div class="skeleton skeleton--text-small" aria-hidden="true"></div>';
+    '<div role="img" class="skeleton skeleton--text-small" aria-hidden="true" aria-label="loading"></div>';
 
 export const textLarge = () =>
-    '<div class="skeleton skeleton--text-large" aria-hidden="true"></div>';
+    '<div role="img" class="skeleton skeleton--text-large" aria-hidden="true" aria-label="loading"></div>';
 
 export const textSmallTwoRow = () =>
-    '<div class="skeleton skeleton--text-small skeleton--text-two-row" aria-hidden="true"></div>';
+    '<div role="img" class="skeleton skeleton--text-small skeleton--text-two-row" aria-hidden="true" aria-label="loading"></div>';
 
 export const textLargeTwoRow = () =>
-    '<div class="skeleton skeleton--text-large skeleton--text-two-row" aria-hidden="true"></div>';
+    '<div role="img" class="skeleton skeleton--text-large skeleton--text-two-row" aria-hidden="true" aria-label="loading"></div>';
+
+export const form = () =>
+    '<div role="img" class="skeleton skeleton--form" aria-hidden="true" aria-label="loading"></div>';
+
+export const image = () =>
+    '<div role="img" class="skeleton skeleton--image" aria-hidden="true" aria-label="loading"></div>';
+
+export const tile = () =>
+    `<div class="skeleton--tile-layout" role="img" aria-hidden="true" aria-label="loading">
+        <div class="skeleton__image-container">
+            <div class="skeleton skeleton--image"></div>
+        </div>
+        <div class="skeleton skeleton--text-large skeleton--text-two-row"></div>
+    </div>`;
