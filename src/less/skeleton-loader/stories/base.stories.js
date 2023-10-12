8bc0b641ba356bb8d4ba89a9619894c71e5bd9d8
Goutham Ponnada <sponnada@ebay.com>
2023-10-12 12:13:57 -0700

    refactor: skeleton tile and layout changes

--- src/less/skeleton-loader/stories/base.stories.js
+++ src/less/skeleton-loader/stories/base.stories.js
@@ -4,13 +4,13 @@ export const avatar = () =>
     '<div role="img" class="skeleton skeleton--avatar" aria-label="loading"></div>';
 
 export const button = () =>
-    '<div role="img" class="skeleton skeleton--button" aria-label="loading"></div>';
+    '<div role="img" class="skeleton skeleton--btn" aria-label="loading"></div>';
 
 export const buttonSmall = () =>
-    '<div role="img" class="skeleton skeleton--button-small" aria-label="loading"></div>';
+    '<div role="img" class="skeleton skeleton--btn-small" aria-label="loading"></div>';
 
 export const buttonLarge = () =>
-    '<div role="img" class="skeleton skeleton--button-large" aria-label="loading"></div>';
+    '<div role="img" class="skeleton skeleton--btn-large" aria-label="loading"></div>';
 
 export const textSmall = () =>
     '<div role="img" class="skeleton skeleton--text-small" aria-label="loading"></div>';
@@ -18,11 +18,11 @@ export const textSmall = () =>
 export const textLarge = () =>
     '<div role="img" class="skeleton skeleton--text-large" aria-label="loading"></div>';
 
-export const textSmallTwoRow = () =>
-    '<div role="img" class="skeleton skeleton--text-small skeleton--text-two-row" aria-label="loading"></div>';
+export const textSmallMultiLine = () =>
+    '<div role="img" class="skeleton skeleton--text-small skeleton--text-multiline" aria-label="loading"></div>';
 
-export const textLargeTwoRow = () =>
-    '<div role="img" class="skeleton skeleton--text-large skeleton--text-two-row" aria-label="loading"></div>';
+export const textLargeMultiLine = () =>
+    '<div role="img" class="skeleton skeleton--text-large skeleton--text-multiline" aria-label="loading"></div>';
 
 export const form = () =>
     '<div role="img" class="skeleton skeleton--form" aria-label="loading"></div>';
@@ -31,9 +31,9 @@ export const image = () =>
     '<div role="img" class="skeleton skeleton--image" aria-label="loading"></div>';
 
 export const tile = () =>
-    `<div class="skeleton--tile-layout" role="img" aria-label="loading">
+    `<div class="skeleton--tile" role="img" aria-label="loading">
         <div class="skeleton__image-container">
-            <div class="skeleton skeleton--image"></div>
+        <div class="skeleton skeleton--image"></div>
         </div>
-        <div class="skeleton skeleton--text-large skeleton--text-two-row"></div>
+        <div class="skeleton skeleton--text-small skeleton--text-multiline"></div>
     </div>`;
