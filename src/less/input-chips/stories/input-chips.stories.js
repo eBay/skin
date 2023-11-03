442e742530722f9a72c79c0e97bd2b210cf30b48
Arthur Khachatryan <arkhachatryan@ebay.com>
2023-11-03 14:22:34 -0700

    feat(input-chips): new module

---
+++ src/less/input-chips/stories/input-chips.stories.js
@@ -0,0 +1,25 @@
+export default { title: "Skin/Input Chips/Base" };
+
+export const simple = () => `
+
+`;
+
+export const expanded = () => `
+
+`;
+
+export const withLabel = () => `
+
+`;
+
+export const withHelperText = () => `
+
+`;
+
+export const disabledState = () => `
+
+`;
+
+export const errorState = () => `
+
+`;
