1bc49a189d2fff120872f237f52587b84ec1e445
Goutham Ponnada <sponnada@ebay.com>
2023-11-14 13:35:09 -0800

    docs(skeletons): updated docs

--- src/less/skeleton/stories/base.stories.js
+++ src/less/skeleton/stories/base.stories.js
@@ -4,13 +4,13 @@ export const avatar = () =>
     '<div role="img" class="skeleton-avatar" aria-label="loading"></div>';
 
 export const button = () =>
-    '<div role="img" class="skeleton-btn" aria-label="loading"></div>';
+    '<div role="img" class="skeleton-button" aria-label="loading"></div>';
 
 export const buttonSmall = () =>
-    '<div role="img" class="skeleton-btn skeleton-btn--small" aria-label="loading"></div>';
+    '<div role="img" class="skeleton-button skeleton-button--small" aria-label="loading"></div>';
 
 export const buttonLarge = () =>
-    '<div role="img" class="skeleton-btn skeleton-btn--large" aria-label="loading"></div>';
+    '<div role="img" class="skeleton-button skeleton-button--large" aria-label="loading"></div>';
 
 export const textSmall = () =>
     '<div role="img" class="skeleton-text skeleton-text--small" aria-label="loading"></div>';
@@ -28,27 +28,19 @@ export const form = () =>
     '<div role="img" class="skeleton-form" aria-label="loading"></div>';
 
 export const smallImage = () =>
-    `<div role="img" class="skeleton" style="height: 50px; width: 50px;">
-        <div class="skeleton-image" aria-label="loading"></div>
-    </div>`;
+    `<div role="img" class="skeleton-image" aria-label="loading" style="height: 50px; width: 50px;"></div>`;
 
 export const image = () =>
-    `<div role="img" class="skeleton" style="height: 250px; width: 250px;">
-        <div class="skeleton-image" aria-label="loading"></div>
-    </div>`;
+    `<div role="img" class="skeleton-image" aria-label="loading" style="height: 250px; width: 250px;"></div>`;
 
 export const tile = () =>
-    `<div class="skeleton" role="img" aria-label="loading" style="width: 225px;">
-        <div class="skeleton__tile">
+    `<div class="skeleton-tile" role="img" aria-label="loading" style="width: 225px;">
         <div class="skeleton-image"></div>
         <div class="skeleton-text skeleton-text--small skeleton-text--multiline"></div>
-        </div>
     </div>`;
 
 export const rtl = () =>
-    `<div class="skeleton" role="img" aria-label="loading" style="width: 225px;" dir="rtl">
-        <div class="skeleton__tile">
+    `<div class="skeleton-tile" role="img" aria-label="loading" style="width: 225px;" dir="rtl">
         <div class="skeleton-image"></div>
         <div class="skeleton-text skeleton-text--small skeleton-text--multiline"></div>
-        </div>
     </div>`;
