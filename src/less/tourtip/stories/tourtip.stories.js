8992bc7e20bc7c0bba201b7f4d183314cd5e0836
Arthur Khachatryan <arkhachatryan@ebay.com>
2023-11-15 09:00:54 -0800

    feat(tourtip): added leading image

--- src/less/tourtip/stories/tourtip.stories.js
+++ src/less/tourtip/stories/tourtip.stories.js
@@ -1,4 +1,4 @@
-export default { title: "Skin/Tourtip" };
+export default { title: "Skin/Tourtip/Base" };
 
 export const collapsed = () => `
 <div class="tourtip">
@@ -92,3 +92,33 @@ export const withOneAction = () => `
         </div>
     </div>
 </div>`;
+
+export const withLeadingImage = () => `
+<div class="tourtip tourtip--expanded">
+    <p class="tourtip__host">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <div class="tourtip__overlay" style="left: calc(50% - 200px); right: auto; top: calc(100% + 12px); bottom: auto" role="region" aria-labelledby="tourtip-label">
+        <span class="tourtip__pointer tourtip__pointer--top"></span>
+        <div class="tourtip__mask">
+            <div class="tourtip__cell">
+                <span class="tourtip__leading">
+                    <img src="https://ir.ebaystatic.com/cr/v/c01/skin/docs/tb-profile-pic.jpg" alt="">
+                </span>
+                <span class="tourtip__content">
+                    <h2 class="tourtip__heading" id="tourtip-label">Tourtip</h2>
+                    <p>Here's something new to help you be successful at your task.</p>
+                </span>
+                <button class="icon-btn icon-btn--transparent tourtip__close" type="button" aria-label="Dismiss tourtip">
+                    <svg class="icon icon--close-16" focusable="false" height="16" width="16" aria-hidden="true">
+                        <use href="#icon-close-16"></use>
+                    </svg>
+                </button>
+                <div class="tourtip__footer">
+                    <span class="tourtip__index">1 / 3</span>
+                    <button class="fake-link">Back</button>
+                    <button class="btn btn--primary">Next</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+`;
