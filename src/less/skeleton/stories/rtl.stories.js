f1291d165f38e7d6d2121bb1caf967e0aa130aef
Goutham Ponnada <saigoutham.p@gmail.com>
2023-12-11 12:31:14 -0800

    Revert "docs(skeletons): added more examples (#2222)"
    This reverts commit 3894029a74252c0e40f89f39449cb1f2d8884456.

--- src/less/skeleton/stories/rtl.stories.js
+++
@@ -1,7 +0,0 @@
-export default { title: "Skin/Skeleton/RTL" };
-
-export const rtl = () =>
-    `<div class="skeleton" role="img" aria-label="loading" style="width: 225px;" dir="rtl">
-        <div class="skeleton__image" style="width: 225px; height: 225px"></div>
-        <div class="skeleton__text  skeleton__text--multiline"></div>
-    </div>`;
