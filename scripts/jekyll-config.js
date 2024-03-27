0f0e1d5a6802a602a5972d33988ab7eb845462c9
Andrew Gliga <agliga4@gmail.com>
2024-03-27 14:38:20 -0700

    chore: fix build version

--- scripts/jekyll-config.js
+++ scripts/jekyll-config.js
@@ -3,15 +3,13 @@
  * Updates the Jekyll front matter defaults with the current skin package version from NPM.
  */
 const fs = require("fs");
+const version = require("../package.json").version;
 const files = ["./docs/_config.yml"];
 
 files.forEach((file) => {
     const newContents = fs
         .readFileSync(file, "utf8")
-        .replace(
-            /version\:.*\n/gi,
-            `version: ${process.env.npm_package_version}\n`
-        );
+        .replace(/version\:.*\n/gi, `version: ${version}\n`);
 
     fs.writeFile(file, newContents, "utf8", (err) => {
         if (err) {
