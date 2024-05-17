e88813a1c5e1983d6930bf75dd46ff99d072276e
Luke LaValva <lukelavalva@gmail.com>
2024-05-17 10:59:46 -0700

    fix(dialog): wrap instead of truncating (#2334)

--- src/less/alert-dialog/stories/alert-dialog.stories.js
+++ src/less/alert-dialog/stories/alert-dialog.stories.js
@@ -31,3 +31,19 @@ export const textSpacing = () => `
     </div>
 </div>
 `;
+
+export const baseWithHeaderOverflow = () => `
+<div aria-labelledby="alert-dialog-title" aria-modal="true" class="alert-dialog alert-dialog--mask-fade" role="alertdialog">
+    <div class="alert-dialog__window alert-dialog__window--fade">
+        <div class="alert-dialog__header">
+            <h2 id="alert-dialog-title" class="alert-dialog__title">Alert! This title is so long that it wraps to the next line. No dialog should ever have a title this long.</h2>
+        </div>
+        <div class="alert-dialog__main">
+            <p id="alert-description-0">You must acknowledge this alert to continue.</p>
+        </div>
+        <div class="alert-dialog__footer">
+            <button class="btn btn--primary alert-dialog__acknowledge" aria-describedby="alert-description-0">OK</button>
+        </div>
+    </div>
+</div>
+`;
