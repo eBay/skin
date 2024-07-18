ea5df35e2f2fba47570823c4a16fbff00d2efb53
Andrew Gliga <agliga4@gmail.com>
2024-07-18 11:36:27 -0700

    feat: updated to use marko-run instead of jekyll

---
+++ .changeset/plenty-coins-burn.md
@@ -0,0 +1,5 @@
+---
+"@ebay/skin": minor
+---
+
+feat: chnaged to use marko run instead of jekyll
---
+++ src/routes/skeleton-examples/examples/csr/example-3/+page.component-browser.js
@@ -0,0 +1,36 @@
+window.addEventListener("load", () => {
+    document.querySelector("button").addEventListener("click", function (e) {
+        // load some skeletons first
+        document.querySelector("main").innerHTML = `
+      <div class="skeleton items" role="img" aria-label="loading">
+        <div>
+          <div class="skeleton__image" style="width: 150px; height: 150px"></div>
+          <div class="skeleton__text skeleton__text--multiline"></div>
+        </div>
+        <div>
+          <div class="skeleton__image" style="width: 150px; height: 150px"></div>
+          <div class="skeleton__text skeleton__text--multiline"></div>
+        </div>
+      </div>
+    `;
+
+        setTimeout(() => {
+            document.querySelector(".skeleton").hidden = true;
+            document.querySelector("button").disabled = true;
+            document.querySelector("main").innerHTML = `
+      <div class="items">
+        <div>
+          <div class="image" style="background-image: url('https://opensource.ebay.com/skin/static/img/tb-landscape-pic.jpg');"></div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget leo et mi cursus rutrum vitae et tortor.</p>
+        </div>
+        <div>
+          <div class="image" style="background-image: url('https://opensource.ebay.com/skin/static/img/tb-landscape-pic.jpg');"></div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget leo et mi cursus rutrum vitae et tortor. Maecenas dictum diam consectetur, luctus elit nec, interdum turpis. Pellentesque ut finibus est. Maecenas ullamcorper bibendum quam.</p>
+        </div>
+      </div>
+      `;
+        }, 3000);
+    });
+});
+
+export default {};
