f1512145fa8f67fa3d263df940858f85b9c6843c
Arthur Khachatryan <arkhachatryan@ebay.com>
2024-07-30 13:00:00 -0700

    fix(toggle-button-group): fixed buttons per row and added min layout exceptions (#2406)

--- src/sass/toggle-button-group/stories/list-layout-exceptions.stories.js
+++ src/sass/toggle-button-group/stories/list-layout-exceptions.stories.js
@@ -2,6 +2,55 @@ export default {
     title: "Skin/Toggle Button Group/List Layout/Layouts Exceptions",
 };
 
+export const minContainer2buttons = () => `
+<div style="width: 280px; border: 1px dashed orange;">
+    <div class="toggle-button-group toggle-button-group--list-layout" data-columns-min="2">
+        <ul aria-label="Buttons">
+            <li>
+                <button type="button" class="toggle-button toggle-button--list-layout" aria-pressed="false">
+                    <span class="toggle-button__content">
+                        <span class="toggle-button__title">Title</span>
+                        <span class="toggle-button__subtitle">Subtitle</span>
+                    </span>
+                </button>
+            </li>
+            <li>
+                <button type="button" class="toggle-button toggle-button--list-layout" aria-pressed="false">
+                    <span class="toggle-button__content">
+                        <span class="toggle-button__title">Title</span>
+                        <span class="toggle-button__subtitle">Subtitle</span>
+                    </span>
+                </button>
+            </li>
+            <li>
+                <button type="button" class="toggle-button toggle-button--list-layout" aria-pressed="false">
+                    <span class="toggle-button__content">
+                        <span class="toggle-button__title">Title</span>
+                        <span class="toggle-button__subtitle">Subtitle</span>
+                    </span>
+                </button>
+            </li>
+            <li>
+                <button type="button" class="toggle-button toggle-button--list-layout" aria-pressed="false">
+                    <span class="toggle-button__content">
+                        <span class="toggle-button__title">Title</span>
+                        <span class="toggle-button__subtitle">Subtitle</span>
+                    </span>
+                </button>
+            </li>
+            <li>
+                <button type="button" class="toggle-button toggle-button--list-layout" aria-pressed="false">
+                    <span class="toggle-button__content">
+                        <span class="toggle-button__title">Title</span>
+                        <span class="toggle-button__subtitle">Subtitle</span>
+                    </span>
+                </button>
+            </li>
+        </ul>
+    </div>
+</div>
+`;
+
 export const _320container2buttons = () => `
 <div style="width: 320px; border: 1px dashed orange;">
     <div class="toggle-button-group toggle-button-group--list-layout" data-columns-xs="2">
