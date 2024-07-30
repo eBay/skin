8a8329802effe1b41c3bf0127caaf75d254fcbf2
Andrew Gliga <agliga4@gmail.com>
2024-07-30 15:45:41 -0700

    chore: addedd script to ineject Y comments

---
+++ scripts/add-y-comments.js
@@ -0,0 +1,33 @@
+// This is a hack added in order to allow the carousel to work in older parsers
+const fs = require("fs");
+const path = require("path");
+const currentDir = path.dirname(__dirname);
+const carousel = path.resolve(currentDir, "dist", "carousel");
+const carouselCSS = path.resolve(carousel, `carousel.css`);
+fs.readFile(carouselCSS, "utf8", function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    const result = data
+        .replace(
+            /\@supports not (.|\n)+?\{/,
+            `@supports not /*!Y */
+      (
+            /*!Y */ (-webkit-scroll-snap-coordinate: 0 0) or /*!Y */
+            (-ms-scroll-snap-coordinate: 0 0) or /*!Y */
+            (scroll-snap-coordinate: 0 0) or /*!Y */ (scroll-snap-align: start)
+      ) {`,
+        )
+        .replace(
+            /\@supports \((.|\n)+?\{/,
+            `@supports (
+    /*!Y */ (-webkit-scroll-snap-coordinate: 0 0) or /*!Y */
+        (-ms-scroll-snap-coordinate: 0 0) or /*!Y */
+        (scroll-snap-coordinate: 0 0) or /*!Y */ (scroll-snap-align: start)
+) {`,
+        );
+
+    fs.writeFile(carouselCSS, result, "utf8", function (err) {
+        if (err) return console.log(err);
+    });
+});
