8dc948dc3bc1ba6351217ef632d79d8ee688ab97
Kenny To <kennytokage@gmail.com>
2024-08-31 21:47:47 -0700

    feat(file-upload): more changes
    fix(file-upload): update css
    
    feat(file-upload): fix css
    
    feat(file-upload): add modules
    
    feat(file-upload): fix module grammar

--- src/sass/card/stories/card-large.stories.js
+++ src/sass/card/stories/card-large.stories.js
@@ -1,8 +1,8 @@
-export default { title: "Skin/Card/Default" };
+export default { title: "Skin/Card/Large" };
 
 export const base = () => `
-    <div class="card">
-        <div class="card__container">
+    <div class="card card--large">
+        <div class="card__container card__container--large">
             <div class="card__content">
                 <svg class="image-placeholder" height="62" width="62" aria-hidden="true">
                     <use href="#image-placeholder"></use>
@@ -13,8 +13,8 @@ export const base = () => `
 `;
 
 export const uploading = () => `
-    <div class="card">
-        <div class="card__container">
+    <div class="card card--large">
+        <div class="card__container card__container--large">
             <div class="card__content">
                 <progress class="progress-bar" value=60 max=100></progress>
             </div>
@@ -28,8 +28,8 @@ export const uploading = () => `
 `;
 
 export const uploadedSquareImage = () => `
-    <div class="card">
-        <div class="card__container">
+    <div class="card card--large">
+        <div class="card__container card__container--large">
             <div class="card__content card__content--image" style="background-image: url(https://ir.ebaystatic.com/cr/v/c01/skin/docs/tb-real-square-pic.jpg)"></div>
             <button class="icon-btn card__action" type="button" aria-label="Open menu">
                 <svg class="icon icon--16" height="16" width="16" aria-hidden="true">
@@ -41,8 +41,8 @@ export const uploadedSquareImage = () => `
 `;
 
 export const uploadedPortraitImage = () => `
-    <div class="card">
-        <div class="card__container">
+    <div class="card card--large">
+        <div class="card__container card__container--large">
             <div class="card__content card__content--image" style="background-image: url(https://ir.ebaystatic.com/cr/v/c01/skin/docs/tb-profile-pic.jpg)"></div>
             <button class="icon-btn card__action" type="button" aria-label="Open menu">
                 <svg class="icon icon--16" height="16" width="16" aria-hidden="true">
@@ -54,8 +54,8 @@ export const uploadedPortraitImage = () => `
 `;
 
 export const uploadedLandscapeImage = () => `
-    <div class="card">
-        <div class="card__container">
+    <div class="card card--large">
+        <div class="card__container card__container--large">
             <div class="card__content card__content--image" style="background-image: url(https://ir.ebaystatic.com/cr/v/c01/skin/docs/tb-landscape-pic.jpg)"></div>
             <button class="icon-btn card__action" type="button" aria-label="Open menu">
                 <svg class="icon icon--16" height="16" width="16" aria-hidden="true">
@@ -67,8 +67,8 @@ export const uploadedLandscapeImage = () => `
 `;
 
 export const uploadedVideo = () => `
-    <div class="card">
-        <div class="card__container">
+    <div class="card card--large">
+        <div class="card__container card__container--large">
             <div class="card__content card__content--image" style="background-image: url(https://ir.ebaystatic.com/cr/v/c01/skin/docs/tb-real-square-pic.jpg)"></div>
             <button class="icon-btn card__action" type="button" aria-label="Open menu">
                 <svg class="icon icon--16" height="16" width="16" aria-hidden="true">
@@ -86,8 +86,8 @@ export const uploadedVideo = () => `
 `;
 
 export const uploadedDocumentWithTitle = () => `
-    <div class="card">
-        <div class="card__container">
+    <div class="card card--large">
+        <div class="card__container card__container--large">
             <div class="card__content card__content--image" style="background-image: url(https://ir.ebaystatic.com/cr/v/c01/skin/docs/tb-landscape-doc.png)"></div>
             <button class="icon-btn card__action" type="button" aria-label="Open menu">
                 <svg class="icon icon--16" height="16" width="16" aria-hidden="true">
@@ -102,8 +102,8 @@ export const uploadedDocumentWithTitle = () => `
 `;
 
 export const uploadedDocumentWithTitleAndDescription = () => `
-    <div class="card">
-        <div class="card__container">
+    <div class="card card--large">
+        <div class="card__container card__container--large">
             <div class="card__content card__content--image" style="background-image: url(https://ir.ebaystatic.com/cr/v/c01/skin/docs/tb-landscape-doc.png)"></div>
             <button class="icon-btn card__action" type="button" aria-label="Open menu">
                 <svg class="icon icon--16" height="16" width="16" aria-hidden="true">
@@ -119,8 +119,8 @@ export const uploadedDocumentWithTitleAndDescription = () => `
 `;
 
 export const uploadedWithOverflow = () => `
-    <div class="card">
-        <div class="card__container">
+    <div class="card card--large">
+        <div class="card__container card__container--large">
             <div class="card__content card__content--image card__content--overflow" style="background-image: url(https://ir.ebaystatic.com/cr/v/c01/skin/docs/tb-real-square-pic.jpg)"></div>
             <button class="icon-btn card__overflow-action" type="button" aria-label="Open menu">
                 <span>+15</span>
--- src/sass/file-upload/stories/file-upload-cards.stories.js
+++ src/sass/file-upload/stories/file-upload-cards.stories.js
@@ -6,23 +6,22 @@ import {
     uploadedDocumentWithTitle as uploadedDocumentWithTitleCard,
     uploadedDocumentWithTitleAndDescription as uploadedDocumentWithTitleAndDescriptionCard,
     uploadedWithOverflow as uploadedWithOverflowCard,
-} from "../../card/stories/card-large.stories";
-
-export default { title: "Skin/File Upload/Compact" };
+} from "../../card/stories/card.stories";
+export default { title: "Skin/File Upload/Default/Cards" };
 
 export const base = () => `
     <div class="file-upload">
         <div class="file-uploader" tabindex="0">
             <label for="file-uploader-input">
-                <div class="file-uploader__container file-uploader__container--compact">
-                    <div class="file-uploader__upload-icon-button file-uploader__upload-icon-button--compact">
+                <div class="file-uploader__container">
+                    <div class="file-uploader__upload-icon-button">
                         <svg class="icon" aria-hidden="true">   
                             <use href="#icon-upload-24"></use>
                         </svg>
                     </div>
                     <div class="file-uploader__content">
-                        <div class="file-uploader__content-header file-uploader__content-header--compact">Add files</div>
-                        <div class="file-uploader__content-subheader file-uploader__content-subheader--compact">Up to 10MB per file in JPG, JPEG, PNG, MP4.</div>
+                        <div class="file-uploader__content-header">Add files</div>
+                        <div class="file-uploader__content-subheader">Up to 10MB per file in JPG, JPEG, PNG, MP4.</div>
                         <button class="fake-link file-uploader__content__button" type="button">Browse files</button>
                     </div>
                 </div>
@@ -36,24 +35,26 @@ export const uploading = () => `
     <div class="file-upload">
         <div class="file-uploader" tabindex="0">
             <label for="file-uploader-input">
-                <div class="file-uploader__container file-uploader__container--compact">
-                    <div class="file-uploader__upload-icon-button file-uploader__upload-icon-button--compact">
+                <div class="file-uploader__container">
+                    <div class="file-uploader__upload-icon-button">
                         <svg class="icon" height="24" width="24" aria-hidden="true">   
                             <use href="#icon-upload-24"></use>
                         </svg>
                     </div>
                     <div class="file-uploader__content">
-                        <div class="file-uploader__content-header file-uploader__content-header--compact">Add files</div>
-                        <div class="file-uploader__content-subheader file-uploader__content-subheader--compact">Up to 10MB per file in JPG, JPEG, PNG, MP4.</div>
+                        <div class="file-uploader__content-header">Add files</div>
+                        <div class="file-uploader__content-subheader">Up to 10MB per file in JPG, JPEG, PNG, MP4.</div>
                         <button class="fake-link file-uploader__content__button" type="button">Browse files</button>
                     </div>
                 </div>
             </label>
             <input id="file-uploader-input" class="file-uploader-input" type="file" />
         </div>
-        <div class="file-upload__assets file-upload__assets--compact">
+        <div class="file-upload__assets">
             ${uploadingCard()}
             ${uploadedPortraitImageCard()}
+            ${uploadedLandscapeImageCard()}
+            ${uploadedVideoCard()}
         </div>
     </div>
 `;
@@ -62,25 +63,33 @@ export const uploadedOverflow = () => `
     <div class="file-upload">
         <div class="file-uploader" tabindex="0">
             <label for="file-uploader-input">
-                <div class="file-uploader__container file-uploader__container--compact">
-                    <div class="file-uploader__upload-icon-button file-uploader__upload-icon-button--compact">
+                <div class="file-uploader__container">
+                    <div class="file-uploader__upload-icon-button">
                         <svg class="icon" height="24" width="24" aria-hidden="true">   
                             <use href="#icon-upload-24"></use>
                         </svg>
                     </div>
                     <div class="file-uploader__content">
-                        <div class="file-uploader__content-header file-uploader__content-header--compact">Add files</div>
-                        <div class="file-uploader__content-subheader file-uploader__content-subheader--compact">Up to 10MB per file in JPG, JPEG, PNG, MP4.</div>
+                        <div class="file-uploader__content-header">Add files</div>
+                        <div class="file-uploader__content-subheader">Up to 10MB per file in JPG, JPEG, PNG, MP4.</div>
                         <button class="fake-link file-uploader__content__button" type="button">Browse files</button>
                     </div>
                 </div>
             </label>
             <input id="file-uploader-input" class="file-uploader-input" type="file" />
         </div>
-        <div class="file-upload__assets file-upload__assets--compact">
+        <div class="file-upload__assets">
             ${uploadedPortraitImageCard()}
             ${uploadedLandscapeImageCard()}
             ${uploadedVideoCard()}
+            ${uploadedPortraitImageCard()}
+            ${uploadedLandscapeImageCard()}
+            ${uploadedVideoCard()}
+            ${uploadedPortraitImageCard()}
+            ${uploadedLandscapeImageCard()}
+            ${uploadedVideoCard()}
+            ${uploadedPortraitImageCard()}
+            ${uploadedLandscapeImageCard()}
             ${uploadedWithOverflowCard()}
         </div>
     </div>
@@ -90,25 +99,26 @@ export const uploadingMixedTypes = () => `
     <div class="file-upload">
         <div class="file-uploader" tabindex="0">
             <label for="file-uploader-input">
-                <div class="file-uploader__container file-uploader__container--compact">
-                    <div class="file-uploader__upload-icon-button file-uploader__upload-icon-button--compact">
+                <div class="file-uploader__container">
+                    <div class="file-uploader__upload-icon-button">
                         <svg class="icon" height="24" width="24" aria-hidden="true">   
                             <use href="#icon-upload-24"></use>
                         </svg>
                     </div>
                     <div class="file-uploader__content">
-                        <div class="file-uploader__content-header file-uploader__content-header--compact">Add files</div>
-                        <div class="file-uploader__content-subheader file-uploader__content-subheader--compact">Up to 10MB per file in JPG, JPEG, PNG, MP4.</div>
+                        <div class="file-uploader__content-header">Add files</div>
+                        <div class="file-uploader__content-subheader">Up to 10MB per file in JPG, JPEG, PNG, MP4.</div>
                         <button class="fake-link file-uploader__content__button" type="button">Browse files</button>
                     </div>
                 </div>
             </label>
             <input id="file-uploader-input" class="file-uploader-input" type="file" />
         </div>
-        <div class="file-upload__assets file-upload__assets--compact">
-            ${uploadedDocumentWithTitleCard()}
+        <div class="file-upload__assets">
             ${uploadedPortraitImageCard()}
+            ${uploadedLandscapeImageCard()}
             ${uploadedVideoCard()}
+            ${uploadedDocumentWithTitleCard()}
             ${uploadedDocumentWithTitleAndDescriptionCard()}
         </div>
     </div>
