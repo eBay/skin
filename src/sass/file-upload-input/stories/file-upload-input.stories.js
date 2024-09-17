805fb41cf979be36ba700a101c5e9601a0cbe587
Kenny To <kennytokage@gmail.com>
2024-09-17 12:44:12 -0700

    feat(file-upload): fix label and add drag style

--- src/sass/file-upload-input/stories/file-upload-input.stories.js
+++ src/sass/file-upload-input/stories/file-upload-input.stories.js
@@ -3,45 +3,68 @@ export default { title: "Skin/File Upload Input" };
 export const base = () => `
     <div class="file-upload-input">
         <input id="file-upload-input-1" class="file-upload-input__input" type="file" multiple/>
-        <label for="file-upload-input-1" class="file-upload-input__label">
-            <div class="file-upload-input__container">
-                <div class="file-upload-input__upload-icon">
-                    <svg class="icon file-upload-input__upload-icon-small" aria-hidden="true">   
-                        <use href="#icon-upload-16"></use>
-                    </svg>
-                    <svg class="icon file-upload-input__upload-icon-large" aria-hidden="true">   
-                        <use href="#icon-upload-24"></use>
-                    </svg>
-                </div>
-                <div class="file-upload-input__content">
-                    <span class="file-upload-input__content-header">Add files</span>
-                    <span class="file-upload-input__content-subheader">Up to 10MB per file in JPG, JPEG, PNG, MP4.</span>
-                    <span class="file-upload-input__content-cta">Browse files</span>
-                </div>
+        <div class="file-upload-input__container">
+            <div class="file-upload-input__upload-icon">
+                <svg class="icon file-upload-input__upload-icon-small" aria-hidden="true">   
+                    <use href="#icon-upload-16"></use>
+                </svg>
+                <svg class="icon file-upload-input__upload-icon-large" aria-hidden="true">   
+                    <use href="#icon-upload-24"></use>
+                </svg>
             </div>
-        </label>
+            <div class="file-upload-input__content">
+                <span class="file-upload-input__content-header">Add files</span>
+                <span class="file-upload-input__content-subheader">Up to 10MB per file in JPG, JPEG, PNG, MP4.</span>
+                <label for="file-upload-input-1">
+                    <span class="file-upload-input__content-cta">Browse files</span>
+                </label>
+            </div>
+        </div>
+    </div>
+`;
+
+export const draggedOver = () => `
+    <div class="file-upload-input">
+        <input id="file-upload-input-1" class="file-upload-input__input" type="file" multiple/>
+        <div class="file-upload-input__container file-upload-input___container--dragged-over">
+            <div class="file-upload-input__upload-icon">
+                <svg class="icon file-upload-input__upload-icon-small" aria-hidden="true">   
+                    <use href="#icon-upload-16"></use>
+                </svg>
+                <svg class="icon file-upload-input__upload-icon-large" aria-hidden="true">   
+                    <use href="#icon-upload-24"></use>
+                </svg>
+            </div>
+            <div class="file-upload-input__content">
+                <span class="file-upload-input__content-header">Add files</span>
+                <span class="file-upload-input__content-subheader">Up to 10MB per file in JPG, JPEG, PNG, MP4.</span>
+                <label for="file-upload-input-1">
+                    <span class="file-upload-input__content-cta">Browse files</span>
+                </label>
+            </div>
+        </div>
     </div>
 `;
 
 export const RTL = () => `
     <div class="file-upload-input" dir="rtl">
         <input id="file-upload-input-1" class="file-upload-input__input" type="file" multiple/>
-        <label for="file-upload-input-1" class="file-upload-input__label">
-            <div class="file-upload-input__container">
-                <div class="file-upload-input__upload-icon">
-                    <svg class="icon file-upload-input__upload-icon-small" aria-hidden="true">   
-                        <use href="#icon-upload-16"></use>
-                    </svg>
-                    <svg class="icon file-upload-input__upload-icon-large" aria-hidden="true">   
-                        <use href="#icon-upload-24"></use>
-                    </svg>
-                </div>
-                <div class="file-upload-input__content">
-                    <span class="file-upload-input__content-header">Add files</span>
-                    <span class="file-upload-input__content-subheader">Up to 10MB per file in JPG, JPEG, PNG, MP4.</span>
-                    <span class="file-upload-input__content-cta">Browse files</span>
-                </div>
+        <div class="file-upload-input__container">
+            <div class="file-upload-input__upload-icon">
+                <svg class="icon file-upload-input__upload-icon-small" aria-hidden="true">   
+                    <use href="#icon-upload-16"></use>
+                </svg>
+                <svg class="icon file-upload-input__upload-icon-large" aria-hidden="true">   
+                    <use href="#icon-upload-24"></use>
+                </svg>
             </div>
-        </label>
+            <div class="file-upload-input__content">
+                <span class="file-upload-input__content-header">Add files</span>
+                <span class="file-upload-input__content-subheader">Up to 10MB per file in JPG, JPEG, PNG, MP4.</span>
+                <label for="file-upload-input-1">
+                    <span class="file-upload-input__content-cta">Browse files</span>
+                </label>
+            </div>
+        </div>
     </div>
 `;
