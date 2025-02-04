102c756e1c4244e83180abae194d1be1a3fc2d88
Arthur Khachatryan <arkhachatryan@ebay.com>
2025-02-04 13:51:02 -0800

    chore(site): updated API to the Algolia approved app/index

--- src/components/search/doc-search.js
+++ src/components/search/doc-search.js
@@ -4,8 +4,8 @@ import docsearch from "@docsearch/js";
 export function init(container) {
     docsearch({
         container,
-        indexName: "skin",
-        appId: "4AG9MNELVM",
-        apiKey: "398f02a5655ce918efcb965b5dc5bcd4",
+        indexName: "opensource-ebay",
+        appId: "XQS94B05ZH",
+        apiKey: "86ffca6d26156df709adfd8d4a44f8b5",
     });
 }
